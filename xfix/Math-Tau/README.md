1ed4db778c0ded9b0962bc3a4ddeddddbd9ba6cd
Moritz Lenz <moritz@faui2k3.org>
2015-02-04 17:50:27 +0100

    git subrepo clone git://github.com/xfix/Math-Tau.git xfix/Math-Tau
    {"local":
      {"subdir":  "xfix/Math-Tau"
      ,"action":  "clone"}
    ,"remote":
      {"url":     "git://github.com/xfix/Math-Tau.git"
      ,"branch":  "master"
      ,"commit":  "e5f5f6d"}
    ,"git-subrepo":
      {"version": "0.1.0"
      ,"commit":  "b6b5c54"
      ,"origin":  "git@github.com:ingydotnet/git-subrepo.git"}}

---
+++ xfix/Math-Tau/README.md
@@ -0,0 +1,10 @@
+2π = τ.
+
+This module exports two constants, `tau` and `τ`. Both are equal to
+2π. See <http://tauday.com/> for more information.
+
+# Example
+
+    use v6;
+    use Math::Tau;
+    say tau + τ;
---
+++ xfix/Math-Tau/lib/Math/Tau.pm
@@ -0,0 +1,9 @@
+# This module may be used under the terms of the MIT license.
+# Copyright © 2014, Konrad Borowski
+
+# The terms of license are available at http://opensource.org/licenses/MIT
+
+use v6;
+
+constant tau is export = 2 * pi;
+constant τ is export = tau;
