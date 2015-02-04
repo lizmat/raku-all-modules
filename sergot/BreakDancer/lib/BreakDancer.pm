beac02dde9cb6fb5875744d752802277e5784a37
Moritz Lenz <moritz@faui2k3.org>
2015-02-04 16:21:29 +0100

    git subrepo clone git://github.com/sergot/BreakDancer.git sergot/BreakDancer
    {"local":
      {"subdir":  "sergot/BreakDancer"
      ,"action":  "clone"}
    ,"remote":
      {"url":     "git://github.com/sergot/BreakDancer.git"
      ,"branch":  "master"
      ,"commit":  "f46f3a1"}
    ,"git-subrepo":
      {"version": "0.1.0"
      ,"commit":  "b6b5c54"
      ,"origin":  "git@github.com:ingydotnet/git-subrepo.git"}}

---
+++ sergot/BreakDancer/lib/BreakDancer.pm
@@ -0,0 +1,37 @@
+module BreakDancer;
+use Shell::Command;
+
+our $basedir = 'www';
+our $ext = '.html';
+
+multi gen($path, &code) is export {
+    mkpath "$basedir/$path";
+    given open("$basedir/$path/index$ext", :w) {
+        .say: &code();
+        .close;
+    }
+}
+
+multi gen($path, @args, &code) is export {
+    mkpath "$basedir/$path";
+    for @args -> $a {
+        my $p = "$basedir/$path/$a";
+        mkpath $p;
+        given open("$p/index$ext", :w) {
+            .say: &code($a);
+            .close;
+        }
+    }
+}
+
+multi gen($path, %args, &code) is export {
+    mkpath "$basedir/$path";
+    for %args.kv -> $k, $v {
+        my $p = "$basedir/$path/$k";
+        mkpath $p;
+        given open("$p/index$ext", :w) {
+            .say: &code($k, $v);
+            .close;
+        }
+    }
+}
