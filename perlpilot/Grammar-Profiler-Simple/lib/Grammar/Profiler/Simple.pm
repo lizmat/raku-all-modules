7050bf9c93fcb32f7fca4b20bc5c2a501af79365
Moritz Lenz <moritz@faui2k3.org>
2015-02-04 16:34:39 +0100

    git subrepo clone git://github.com/perlpilot/Grammar-Profiler-Simple.git perlpilot/Grammar-Profiler-Simple
    {"local":
      {"subdir":  "perlpilot/Grammar-Profiler-Simple"
      ,"action":  "clone"}
    ,"remote":
      {"url":     "git://github.com/perlpilot/Grammar-Profiler-Simple.git"
      ,"branch":  "master"
      ,"commit":  "14bbe29"}
    ,"git-subrepo":
      {"version": "0.1.0"
      ,"commit":  "b6b5c54"
      ,"origin":  "git@github.com:ingydotnet/git-subrepo.git"}}

---
+++ perlpilot/Grammar-Profiler-Simple/lib/Grammar/Profiler/Simple.pm
@@ -0,0 +1,40 @@
+my %timing;
+
+my class ProfiledGrammarHOW is Metamodel::GrammarHOW {
+
+    method find_method($obj, $name) {
+        my $meth := callsame;
+        substr($name, 0, 1) eq '!' ||
+        substr($name, 0, 8) eq 'dispatch' || 
+        $name eq any(« parse CREATE Bool defined MATCH Stringy Str WHERE orig BUILD DESTROY ») ??
+            $meth !!
+            -> $c, |args {
+                my $grammar = $obj.^name;
+                %timing{$grammar} //= {};                   # Vivify grammar hash
+                %timing{$grammar}{$meth.name} //= {};       # Vivify method hash
+                my %t := %timing{$grammar}{$meth.name};
+                my $start = now;
+                my $result := $meth($obj, |args);
+                %t<time> += now - $start;
+                %t<calls>++;
+                $result
+            }
+    }
+
+    method publish_method_cache($obj) {
+        # no caching, so we always hit find_method
+    }
+}
+
+proto sub get-timing (|) is export { * }
+multi sub get-timing () { %timing }
+multi sub get-timing ($grammar) { %timing{$grammar} }
+multi sub get-timing ($grammar, $rule) { %timing{$grammar}{$rule} }
+
+proto sub reset-timing (|) is export { * }
+multi sub reset-timing () { %timing = () }
+multi sub reset-timing ($grammar) { %timing{$grammar} = () }
+multi sub reset-timing ($grammar, $rule) { %timing{$grammar}{$rule} = () }
+
+my module EXPORTHOW { }
+EXPORTHOW.WHO.<grammar> = ProfiledGrammarHOW;
