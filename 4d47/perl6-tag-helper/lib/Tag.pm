3d14e360e55f30f8129b2cc8d16d7854bf112a8e
Moritz Lenz <moritz@faui2k3.org>
2015-02-04 18:37:30 +0100

    git subrepo clone git://github.com/4d47/perl6-tag-helper.git 4d47/perl6-tag-helper
    {"local":
      {"subdir":  "4d47/perl6-tag-helper"
      ,"action":  "clone"}
    ,"remote":
      {"url":     "git://github.com/4d47/perl6-tag-helper.git"
      ,"branch":  "master"
      ,"commit":  "3331269"}
    ,"git-subrepo":
      {"version": "0.1.0"
      ,"commit":  "b6b5c54"
      ,"origin":  "git@github.com:ingydotnet/git-subrepo.git"}}

---
+++ 4d47/perl6-tag-helper/lib/Tag.pm
@@ -0,0 +1,66 @@
+class Tag;
+
+use HTML::Entity;
+
+#| Assign ' /' for XHTML
+our $.self-closing-marker = '';
+
+#| Positional of elements that dont have any embed content
+our @.void-elements = < area base br col command embed hr img input gen link meta param source track wbr >;
+
+#| Parcel of attributes that dont have any content
+our @.boolean-attributes = < async checked compact declare defer disabled ismap multiple noresize noshade nowrap open readonly required reversed scoped selected >;
+
+has $!value;
+
+submethod BUILD(:$!value = '') {
+    # allow private attribute in default constructor
+}
+
+method Str {
+    $!value
+}
+
+method gist {
+    self.Str
+}
+
+method FALLBACK(Str $name, *@args, *%attrs) {
+    my $attr = '';
+    my $value = self;
+
+    # build attr string out of hash
+    for %attrs.kv -> $k, $v {
+        if @.boolean-attributes.any ~~ $k {
+            if $v {
+                $attr ~= $.self-closing-marker ?? " $k=\"$k\"" !! " $k";
+            }
+        } else {
+            $attr ~= " $k=\"{ encode-entities($v) }\""
+        }
+    }
+
+    # encode everything that is not a Tag object
+    @args = @args.map: -> $e {
+        $e !~~ Tag ?? encode-entities($e) !! $e;
+    };
+
+    # build tag
+    given $name {
+        when /^begin_/ {
+            $value ~= "<{ $name.substr(6) ~ $attr }>";
+        }
+        when /^end_/ {
+            $value ~= "</{ $name.substr(4) }>";
+        }
+        when @.void-elements.any {
+            $value ~= "<$name$attr$.self-closing-marker>";
+        }
+        default {
+            $value ~= "<$name$attr>{@args}</$name>";
+        }
+    }
+
+    Tag.new(:$value);
+}
+
