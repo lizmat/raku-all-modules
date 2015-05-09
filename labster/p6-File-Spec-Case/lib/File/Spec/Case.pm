c19e9381d8e9b098b1b87f1ace437adfb9c16167
Moritz Lenz <moritz@faui2k3.org>
2015-05-09 18:28:49 +0200

    git subrepo clone --force git://github.com/labster/p6-File-Spec-Case.git labster/p6-File-Spec-Case
    subrepo:
      subdir:   "labster/p6-File-Spec-Case"
      merged:   "0845ee4"
    upstream:
      origin:   "git://github.com/labster/p6-File-Spec-Case.git"
      branch:   "master"
      commit:   "0845ee4"
    git-subrepo:
      version:  "0.2.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b136967"

--- labster/p6-File-Spec-Case/lib/File/Spec/Case.pm
+++ labster/p6-File-Spec-Case/lib/File/Spec/Case.pm
@@ -1,41 +1,41 @@
 class File::Spec::Case;
 
 method default-case-tolerant ($OS = $*OS) {
-	so $OS eq any <MacOS Mac VMS darwin Win32 MSWin32 os2 dos NetWare symbian cygwin Cygwin epoc>;
+    so $OS eq any <MacOS Mac VMS darwin Win32 MSWin32 os2 dos NetWare symbian cygwin Cygwin epoc>;
 }
 
 method always-case-tolerant  ($OS = $*OS) {
-	so $OS eq any <MacOS Mac VMS os2 dos>;
+    so $OS eq any <MacOS Mac VMS os2 dos>;
 }
 
 method sensitive(|c)   { not self.tolerant( |c ) }
 method insensitive(|c) {     self.tolerant( |c ) }
 
 method tolerant (Cool:D $path is copy = ~$*CWD, :$no_write = False ) {
-	return True if self.always-case-tolerant($*OS);
+    return True if self.always-case-tolerant($*OS);
 
     $path = $path.IO;
-	$path.e or fail "Invalid path given";
-	
-	if $path.f and $path.basename ~~ /<+upper+lower-[\x00DF]>/ {
-	    return self!case-tolerant-file($path);
-	}
+    $path.e or fail "Invalid path given";
+
+    if $path.f and $path.basename ~~ /<+upper+lower-[\x00DF]>/ {
+        return self!case-tolerant-file($path);
+    }
 
     # try looking at everything in the current dir for letters
-	$path = $path.parent unless $path.d;
+    $path = $path.parent unless $path.d;
     for $path.dir -> $fn {
         if $fn.basename ~~ /<+upper+lower-[\x00DF]>/ {
-            return self!case-tolerant-file($path);
+            return self!case-tolerant-file($fn);
         }
     }
-	
-	
-	#if nothing in $path contains a letter, try writing a test file	
-	unless $no_write and !$path.w {
+
+
+    #if nothing in $path contains a letter, try writing a test file
+    unless $no_write and !$path.w {
         # we already know this dir don't contain <lower+upper>,
         # so pick a random 8.3 name to avoid race conditions
         my $tmpname = "{('a'..'z').pick(8).join}.tmp";
-        my $filelc = $path.child($tmpname);  
+        my $filelc = $path.child($tmpname);
         my $fileuc = $path.child($tmpname.uc);
         try {
             $filelc.spurt: :createonly,
@@ -45,22 +45,22 @@ method tolerant (Cool:D $path is copy = ~$*CWD, :$no_write = False ) {
             return $result;
         }
         CATCH { unlink $filelc if $filelc.e; }
-	}
+    }
 
-	# Okay, we don't have write access... give up and just return the platform default
-	return self.default-case-tolerant($*OS);
+    # Okay, we don't have write access... give up and just return the platform default
+    return self.default-case-tolerant($*OS);
 
 }
 
 method !case-tolerant-file( $path ) {
     my ($volume, $dirname, $basename) = $path.parts<volume dirname basename>;
-    
+
     return False unless
            IO::Path.new( :$volume, :$dirname, basename => $basename.uc ).e
         && IO::Path.new( :$volume, :$dirname, basename => $basename.lc ).e;
-    return +$path.parent.contents».basename.grep(/:i ^ {$path.basename} $/) <= 1;
-	# this could be faster by comparing inodes of .uc and .lc
-	# but we can't guarantee POSIXness of every platform that calls this
+    return +$path.parent.dir».basename.grep(/:i ^ {$path.basename} $/) <= 1;
+    # this could be faster by comparing inodes of .uc and .lc
+    # but we can't guarantee POSIXness of every platform that calls this
 }
 
 
--- labster/p6-File-Spec-Case/t/00_basic.t
+++ labster/p6-File-Spec-Case/t/00_basic.t
@@ -2,17 +2,19 @@ use v6;
 use Test;
 use File::Spec::Case;
 
-plan 5;
+plan 7;
 
 if ($*CWD.IO ~~ :w) {
-	"casetol.tmp".IO.e or spurt "casetol.tmp", "temporary test file, delete after reading";
+	ok( ("casetol.tmp".IO.e or spurt("casetol.tmp", "temporary test file, delete after reading")),
+        "created test file");
+    ok "casetol.tmp".IO.e, "test file exists";
 	is File::Spec::Case.tolerant("casetol.tmp"), so "CASETOL.TMP".IO.e,
 		"tolerant is {so "CASETOL.TMP".IO.e} in cwd";
-	is File::Spec::Case.insensitive, so "CASETOL.TMP".IO.e, "insensitive ok";
-	isnt File::Spec::Case.sensitive, so "CASETOL.TMP".IO.e, "sensitive ok";
+	ok   File::Spec::Case.insensitive === "CASETOL.TMP".IO.e, "insensitive ok";
+	nok  File::Spec::Case.\ sensitive === "CASETOL.TMP".IO.e, "sensitive ok";
 	unlink "casetol.tmp";
 }
-else { skip "tolerant/sensitive/insensitive, no write access in cwd", 3; } 
+else { skip "tolerant/sensitive/insensitive, no write access in cwd", 6; } 
 
 ok File::Spec::Case.always-case-tolerant("dos"), "always-case-tolerant ok";
 ok File::Spec::Case.default-case-tolerant("darwin"), "default-case-tolerant ok";
