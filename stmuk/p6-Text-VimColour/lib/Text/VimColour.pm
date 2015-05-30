f043a025563a58cfe67c7c583dea0e57c8cbe07b
Moritz Lenz <moritz@faui2k3.org>
2015-05-30 17:28:13 +0200

    git subrepo clone --force git://github.com/stmuk/p6-Text-VimColour.git stmuk/p6-Text-VimColour
    subrepo:
      subdir:   "stmuk/p6-Text-VimColour"
      merged:   "abadc3d"
    upstream:
      origin:   "git://github.com/stmuk/p6-Text-VimColour.git"
      branch:   "master"
      commit:   "abadc3d"
    git-subrepo:
      version:  "0.2.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b136967"

--- stmuk/p6-Text-VimColour/lib/Text/VimColour.pm
+++ stmuk/p6-Text-VimColour/lib/Text/VimColour.pm
@@ -1,15 +1,54 @@
 #!/usr/bin/env perl6
-
 use v6;
-
+use File::Temp;
 class Text::VimColour:ver<0.1> {
-
-    method BUILD(:$lang, :$in, :$out) {
-        my $version = q:x/ex --version/;
-        $version.=split("\n")[0];
-        die "didn't find a recent vim/ex"  unless $version ~~ /' Vi IMproved 7.4 '/;
-        my $exit = shell "ex -c 'set bg=light|set ft=$lang|TOhtml|wq! $out|quit' $in 2>&1 > /dev/null";
-
-        die "fail" unless $exit==0;
+    subset File of Str where -> $x { so $x && $x.IO.e };
+    subset Path of Str where -> $x { so $x && $x.IO.dirname.IO.e } 
+    has Path  $!out;
+    has File  $!in;
+    has Str   $!lang;
+    
+    proto method BUILD(|z) {
+	ENTER {
+	    my $version = .shift given split /','/, q:x/ex --version/;
+	    die "didn't find a recent vim/ex"  unless $version ~~ /' Vi IMproved 7.4 '/;
+	    callsame;
+	}
+	{*}
+	LEAVE {
+	    $!lang //= 'c';
+	    my $cmd = qq«
+		vim -c 'set bg=light|set ft=$!lang|TOhtml|wq! $!out|quit' $!in 2>/dev/null >/dev/null 
+            »;
+	    my $proc = shell $cmd;
+	    fail "failed to run '$cmd', exit code {$proc.exitcode}" unless $proc.exitcode == 0;
+	}
     }
+    
+    multi method BUILD(Str :$!lang, File :$!in,  Path :$!out) {};
+    
+    multi method BUILD(Str :$!lang, File :$!in) {
+	$!out = tempfile[0];
+
+    }
+    multi method BUILD(Str  :$!lang, Str :$code where $code.chars > 0) {
+	$!in  = tempfile[0];
+	$!in.IO.spurt: $code;
+	$!out = tempfile[0];
+
+    }
+    
+    method html-full-page returns Str{
+	$!out.IO.slurp;
+    }
+    method html  {
+	my $html = $!out.IO.slurp;
+	$html ~~  m/  '<body>'  (.*) '</body>' / && ~$0;
+    }
+
+    method css   {
+	my $html = $!out.IO.slurp;
+	$html ~~  m/  '<style type="text/css">'  (.*?) '</style>' / && ~$0;
+    }
+    
 }
--- stmuk/p6-Text-VimColour/t/vim_colour.t
+++ stmuk/p6-Text-VimColour/t/vim_colour.t
@@ -7,12 +7,25 @@ BEGIN { @*INC.unshift( 'lib' ) }
 use Test;
 use Text::VimColour;
 
-plan 1;
+plan 5;
 
 my $lang = 'perl6';
 my $in = 't/vim_colour.t';
-my $out = 't/index.html';
+my $out = $?FILE.IO.dirname ~ $*SPEC.dir-sep ~ 'index.html';
+
+unlink $out if $out.IO.f;
 
 Text::VimColour.new( :$lang, :$in, :$out ) ;
+ok $out.IO.e && slurp($out) ~~ /vimCodeElement/, 'found vimCodeElement';
 
-ok slurp($out) ~~ /vimCodeElement/, 'found vimCodeElement';
+my $c = Text::VimColour.new(:$lang, :$in);
+ok $c.html ~~ /vimCodeElement/, 'to temp file';
+
+my $x = Text::VimColour.new(:lang('perl6'), code => 'use v6; BEGIN {}; ');
+ok $x.html-full-page ~~ /vimCodeElement/, 'from string';
+
+ok $x.html ~~ /vimCodeElement/, 'body';
+
+ok $x.css ~~ /background/, 'css';
+
+
