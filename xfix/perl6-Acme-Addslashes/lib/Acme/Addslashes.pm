1ca078647b8cc203554905691bff8eec4781d0a9
Moritz Lenz <moritz@faui2k3.org>
2015-05-30 17:13:58 +0200

    git subrepo clone --force git://github.com/xfix/perl6-Acme-Addslashes.git xfix/perl6-Acme-Addslashes
    subrepo:
      subdir:   "xfix/perl6-Acme-Addslashes"
      merged:   "ff2907d"
    upstream:
      origin:   "git://github.com/xfix/perl6-Acme-Addslashes.git"
      branch:   "master"
      commit:   "ff2907d"
    git-subrepo:
      version:  "0.2.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b136967"

--- xfix/perl6-Acme-Addslashes/lib/Acme/Addslashes.pm
+++ xfix/perl6-Acme-Addslashes/lib/Acme/Addslashes.pm
@@ -1,4 +1,4 @@
-module Acme::Addslashes;
+unit module Acme::Addslashes;
 sub addslashes(Cool $string is copy) is export {
     $string = ~$string;
     $string ~~ s:g/(.)/$0\c[COMBINING LONG SOLIDUS OVERLAY]/;
