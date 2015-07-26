eb85e055d62295b1d685ca01c6e4f7dd335ceb71
Moritz Lenz <moritz@faui2k3.org>
2015-07-26 18:08:42 +0200

    git subrepo clone --force git://github.com/stmuk/p6-Text-VimColour.git stmuk/p6-Text-VimColour
    subrepo:
      subdir:   "stmuk/p6-Text-VimColour"
      merged:   "b77254f"
    upstream:
      origin:   "git://github.com/stmuk/p6-Text-VimColour.git"
      branch:   "master"
      commit:   "b77254f"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- stmuk/p6-Text-VimColour/lib/Text/VimColour.pm
+++ stmuk/p6-Text-VimColour/lib/Text/VimColour.pm
@@ -2,53 +2,51 @@
 use v6;
 use File::Temp;
 
-class Text::VimColour:ver<0.3> {
+class Text::VimColour:ver<0.4> {
     subset File of Str where -> $x { so $x && $x.IO.e };
     subset Path of Str where -> $x { so $x && $x.IO.dirname.IO.e } 
-    # BUG https://rt.perl.org/Public/Bug/Display.html?id=125245
-    # has Path  $!out;
-    # has File  $!in;
-    # has Str   $!lang;
-    has $!out;
-    has $!in;
-    has $!lang;
-    
-    my $html;
+    has Path  $!out;
+    has File  $!in;
+    has Str   $!lang;
+
     proto method BUILD(|z) {
-	ENTER {
-	    my $version = .shift given split /','/, q:x/ex --version/;
-	    die "didn't find a recent vim/ex"  unless $version ~~ /' Vi IMproved 7.4 '/;
-	    callsame;
-	}
-	{*}
-	LEAVE {
-	    $!lang //= 'c';
+        ENTER {
+            my $version = .shift given split /','/, q:x/ex --version/;
+            die "didn't find a recent vim/ex"  unless $version ~~ /' Vi IMproved 7.4 '/;
+            callsame;
+        }
+        {*}
+        LEAVE {
+            $!lang //= 'c';
             my $vim-let = $*VIM-LET || "";
-        
-        %*ENV<EXRC>="set directory=/tmp";
-	    my $cmd = qq«
-		vim -E -T builtin_dumb -c 'syntax on|set noswapfile|set bg=light|set ft=$!lang|{$vim-let}|TOhtml|wq! $!out|quit' $!in 2>/dev/null >/dev/null
+
+            %*ENV<EXRC>="set directory=/tmp";
+            my $cmd = qq«
+                vim -E -s -c 'let g:html_no_progress=1|syntax on|set noswapfile|set bg=light|set ft=$!lang|{$vim-let}|runtime syntax/2html.vim|wq! $!out|quit' -cqa $!in 2>/dev/null >/dev/null
             »;
             my $proc = shell $cmd;
-	    fail "failed to run '$cmd', exit code {$proc.exitcode}" unless $proc.exitcode == 0;
-	}
+            fail "failed to run '$cmd', exit code {$proc.exitcode}" unless $proc.exitcode == 0;
+        }
     }
-    
+
     multi method BUILD(Str :$!lang, File :$!in,  Path :$!out) {};
-    
+
     multi method BUILD(Str :$!lang, File :$!in) {
-	$!out = tempfile[0];
+        $!out = tempfile[0];
 
     }
+
     multi method BUILD(Str  :$!lang, Str :$code where $code.chars > 0) {
-	$!in  = tempfile[0];
+        $!in  = tempfile[0];
         $!in.IO.spurt: $code;
         $!out = tempfile[0];
 
     }
+
     method html-full-page returns Str  {
         $!out.IO.slurp;
     }
+
     method html returns Str  {
         self.html-full-page ~~  m/  '<body' .*? '>'  (.*) '</body>' / ;
         return ~$0;
@@ -58,6 +56,3 @@ class Text::VimColour:ver<0.3> {
         self.html-full-page ~~  m/  '<style type="text/css">'  (.*?) '</style>' / && ~$0;
     }
 }
-
-
-
