db3547bbb43124d713bb044233a4e7f32ec782df
Moritz Lenz <moritz@faui2k3.org>
2015-07-26 18:16:51 +0000

    git subrepo clone --force https://github.com/flussence/BufUtils.git flussence/BufUtils
    subrepo:
      subdir:   "flussence/BufUtils"
      merged:   "40bae4d"
    upstream:
      origin:   "https://github.com/flussence/BufUtils.git"
      branch:   "master"
      commit:   "40bae4d"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- flussence/BufUtils/lib/BufUtils.pm
+++ flussence/BufUtils/lib/BufUtils.pm
@@ -1,25 +1,40 @@
-unit module BufUtils:auth<github:flussence>:ver<0.0.1>;
+unit module BufUtils:auth<github:flussence>:ver<0.0.2>;
 
-our sub infix:<x>(Stringy \piece, Int \count) returns Stringy is export {
+our sub infix:<x>(Stringy \piece, Int \count --> Stringy) is export {
     [~] piece xx count
 }
 
 our sub index(
     Blob $haystack,
     Blob $needle,
-    Cool $startpos = 0) returns Int is export {
+    Cool $startpos = 0 --> Int) is export {
     return Int if $haystack.elems - $startpos < $needle.elems;
 
     # This is a really sucky brute-force search, but it'll do for demonstration
     return ($startpos..$haystack.elems - $needle.elems).first({
-        $haystack.subbuf($_, $needle.elems) eq $needle;
+        $haystack.subbuf($^offset, $needle.elems) eq $needle;
     }) // Int;
 }
 
-our sub starts-with(Blob $b, Blob $needle) returns Bool:D is export {
-    $b.subbuf(0, $needle.elems) eq $needle;
+our sub starts-with(Blob $_, Blob $needle --> Bool:D) is export {
+    .subbuf(0, $needle.elems) eq $needle;
 }
 
-our sub ends-with(Blob $b, Blob $needle) returns Bool:D is export {
-    $b.subbuf($b.elems - $needle.elems) eq $needle;
+our sub ends-with(Blob $_, Blob $needle --> Bool:D) is export {
+    .subbuf(.elems - $needle.elems) eq $needle;
 }
+
+our sub chomp(Blob $_ --> Blob) is export {
+    my $eol = "\n".encode;
+
+    ends-with($_, $eol) ?? .subbuf(0, .elems - $eol.bytes)
+                        !! $_;
+}
+
+our sub lc(Blob $_ --> Blob) is export { .decode.lc.encode }
+our sub uc(Blob $_ --> Blob) is export { .decode.uc.encode }
+our sub tc(Blob $_ --> Blob) is export { .decode.tc.encode }
+our sub tclc(Blob $_ --> Blob) is export { .decode.tclc.encode }
+
+our sub unival(utf8 $_ --> Numeric) is export { .decode('utf-8').unival }
+our sub univals(utf8 $_ --> List) is export { .decode('utf-8').univals }
