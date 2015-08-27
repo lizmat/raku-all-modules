7c130e170b717488d1f610c80f17dd0007b878c3
Moritz Lenz <moritz@faui2k3.org>
2015-08-27 20:26:45 +0000

    git subrepo clone --force git://github.com/tadzik/perl6-Acme-Meow.git tadzik/perl6-Acme-Meow
    subrepo:
      subdir:   "tadzik/perl6-Acme-Meow"
      merged:   "5ba5526"
    upstream:
      origin:   "git://github.com/tadzik/perl6-Acme-Meow.git"
      branch:   "master"
      commit:   "5ba5526"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- tadzik/perl6-Acme-Meow/lib/Acme/Meow.pm
+++ tadzik/perl6-Acme-Meow/lib/Acme/Meow.pm
@@ -3,57 +3,57 @@ use v6;
 #= Our cute, artificial cat (perfect for allergics!)
 class Acme::Meow {
 
-	has Numeric $!love = 0;
-	has Str     $!fav = '';
+    has Numeric $!love = 0;
+    has Str     $!fav = '';
 
     #= pet our kitty
-	method pet {
-		$!love++;
-		$!fav = <milk nip>.pick;
-		say ~[self!kitty_status, <purr nuzzle meow>.pick,
-			$!love > 15 ?? '<3' !! ''];
-	}
+    method pet {
+        $!love++;
+        $!fav = <milk nip>.pick;
+        say ~[self!kitty_status, <purr nuzzle meow>.pick,
+            $!love > 15 ?? '<3' !! ''];
+    }
 
     #= feed our kitty
-	method feed($food?) {
-		if self.is_sleeping {
-			$!love += 0.25
-		} else {
-			$!love += 0.5
-		}
-		if $food.defined {
-			if $food eq 'nip' {
-				say ~[self!kitty_status, 'crunch']
-			}
-			elsif $food eq 'milk' {
-				say ~[self!kitty_status, 'lap lap']
-			}
-		} else {
-			say ~[self!kitty_status, ['crunch', 'lap lap', ''].pick]
-		}
-	}
+    method feed($food?) {
+        if self.is_sleeping {
+            $!love += 0.25
+        } else {
+            $!love += 0.5
+        }
+        if $food.defined {
+            if $food eq 'nip' {
+                say ~[self!kitty_status, 'crunch']
+            }
+            elsif $food eq 'milk' {
+                say ~[self!kitty_status, 'lap lap']
+            }
+        } else {
+            say ~[self!kitty_status, ['crunch', 'lap lap', ''].pick]
+        }
+    }
 
     #= is our kitty sleeping?
-	method is_sleeping {
-		False # cats sleep? They're just pretending.
-		      # They're watching you. All the time.
-		      # or TODO, if you prefer
-	}
+    method is_sleeping {
+        False # cats sleep? They're just pretending.
+              # They're watching you. All the time.
+              # or TODO, if you prefer
+    }
 
     #= handy method to feed your cat with a nip
-	method nip {
-		self.feed: 'nip'
-	}
+    method nip {
+        self.feed: 'nip'
+    }
 
     #= another one to give some milk to our kitty
-	method milk {
-		self.feed: 'milk'
-	}
+    method milk {
+        self.feed: 'milk'
+    }
 
-	method !kitty_status {
-		return 'zZzZ' if self.is_sleeping;
-		$!love > 5 ?? '=^_^=' !! '=-_-='
-	}
+    method !kitty_status {
+        return 'zZzZ' if self.is_sleeping;
+        $!love > 5 ?? '=^_^=' !! '=-_-='
+    }
 }
 
 =begin pod
