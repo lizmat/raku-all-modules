2b6aebb2c09959644ac8261527064a5155eb70bd
Moritz Lenz <moritz@faui2k3.org>
2015-10-30 17:19:53 +0000

    git subrepo clone --force git://github.com/TiMBuS/Net--IRC.git TiMBuS/Net--IRC
    subrepo:
      subdir:   "TiMBuS/Net--IRC"
      merged:   "72d9b61"
    upstream:
      origin:   "git://github.com/TiMBuS/Net--IRC.git"
      branch:   "master"
      commit:   "72d9b61"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- TiMBuS/Net--IRC/lib/Net/IRC/CommandHandler.pm
+++ TiMBuS/Net--IRC/lib/Net/IRC/CommandHandler.pm
@@ -6,41 +6,63 @@ enum RequiredIntro is export <
 	PREFIX
 	EITHER
 	BOTH
-	>;
+>;
+
+enum CommandType is export <Short Long>;
+role Command { has $.abbreviate };
+multi trait_mod:<is>(Routine:D $m, :cmd(:$command)!) is export {
+	$m does Command(abbreviate => ($command !eqv Long));
+}
+
+sub abbrev($name) { [\~] $name.comb }
 
 role Net::IRC::CommandHandler {
 	has Str $.prefix is rw = '!';
 	has RequiredIntro $.required-intro is rw = EITHER;
 
+	has @!cmds       = self.^methods.grep(Command);
+	has %cmd-names   = @!cmds.map({ $^n.name => $^n });
+	has %short-names = {}.push(
+		@!cmds.grep(*.abbreviate).map({ abbrev($^n.name) X=> $^n }).flat
+	);
+
 	method recognized($handler: $ev) {
-		return $ev.cache<CommandHandler>{$handler.prefix} //= (gather {
+		return $ev.cache<CommandHandler>{$handler.prefix} //= sub {
 			$ev.what ~~ token {
 				# Intro
 				^
-				[ \s* $<nick>=("$ev.state()<nick>") [ ':' | ',' | \s ] ]? \s*
+				[ \s* $<nick>=("$ev.state()<nick>") [ <[:,]> | \s ] ]? \s*
 				[ $<prefix>=("$handler.prefix()") \s* ]?
 
 				# Actual command (and optional params)
-				$<command>=(\w+) [ <?> | \s+ $<params>=(.*) ]
+				$<command>=(\w+) [ <?> | \s+ $<params>=(.*) ] #++# <- Fool sublime's perl parser
 				$
-			} or take False;
+			} or return False;
 
 			# Let private chat act as specifying the bot's nick
 			my $nick = $<nick> || $ev.where eq $ev.state<nick>;
 
 			given $.required-intro {
-				when NICK   { take False unless $nick		   }
-				when PREFIX { take False unless $<prefix>	   }
-				when EITHER { take False unless $<prefix> || $nick }
-				when BOTH   { take False unless $<prefix> && $nick }
+				when NICK   { return False unless $nick              }
+				when PREFIX { return False unless $<prefix>          }
+				when EITHER { return False unless $<prefix> || $nick }
+				when BOTH   { return False unless $<prefix> && $nick }
 			}
 
-			take $/;
-		})[0];
+			return $/;
+		}();
 	}
 
 	multi method said ($ev where { $/ := $.recognized($ev) }) {
-		self.*"command_$<command>"($ev, $/);
+		self!dispatch($<command>, $ev, $/);
+	}
+
+	method !dispatch($name, *@args) {
+		given %cmd-names{$name} // %short-names{$name} {
+			when Callable   { .(self, |@args) }
+			when Positional { warn "Cannot disambiguate '$name'. Possible commands: {$_>>.name.join(', ')}" }
+			default         { warn 'Nothing to dispatch!' }
+		}
 	}
 
 	method usage($ev, $usage) {
