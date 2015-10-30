eb4c3c941df885855e2b2c9e2603e264556da937
Moritz Lenz <moritz@faui2k3.org>
2015-10-30 17:24:12 +0000

    git subrepo clone --force git://github.com/perlpilot/p6-File-Temp.git perlpilot/p6-File-Temp
    subrepo:
      subdir:   "perlpilot/p6-File-Temp"
      merged:   "cef567c"
    upstream:
      origin:   "git://github.com/perlpilot/p6-File-Temp.git"
      branch:   "master"
      commit:   "cef567c"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- perlpilot/p6-File-Temp/lib/File/Temp.pm
+++ perlpilot/p6-File-Temp/lib/File/Temp.pm
@@ -12,6 +12,27 @@ sub gen-random($n) {
 
 my @open-files;
 
+sub make-temp($type, $template, $tempdir, $prefix, $suffix, $unlink) {
+    my $count = MAX-RETRIES;
+    while ($count--) {
+        my $tempfile = $template;
+        $tempfile ~~ s/ '*' ** 4..* /{ gen-random($/.chars) }/;
+        my $name = $*SPEC.catfile($tempdir,"$prefix$tempfile$suffix");
+        next if $name.IO ~~ :e;
+        my $fh;
+        if $type eq 'file' {
+            $fh = try { CATCH { next }; open $name, :rw, :exclusive;  };
+        }
+        else {
+            try { CATCH { next }; mkdir($name) };
+        }
+        push @open-files, $name if $unlink;
+        return $type eq 'file' ?? ($name,$fh) !! $name;
+    }
+    return ();
+}
+
+
 sub tempfile (
     $tmpl? = '*' x 10,          # positional template
     :$tempdir? = $*TMPDIR,      # where to create these temp files
@@ -20,39 +41,18 @@ sub tempfile (
     :$unlink?  = 1,             # remove when program exits?
     :$template = $tmpl          # required named template
 ) is export {
-
-    my $count = MAX-RETRIES;
-    while ($count--) {
-        my $tempfile = $template;
-        $tempfile ~~ s/ '*' ** 4..* /{ gen-random($/.chars) }/;
-        my $filename = "$tempdir/$prefix$tempfile$suffix";
-        next if $filename.IO ~~ :e;
-        my $fh = try { CATCH { next }; open $filename, :rw;  };
-        push @open-files, $filename if $unlink;
-        return $filename,$fh;
-    }
-    return ();
+    return make-temp('file', $template, $tempdir, $prefix, $suffix, $unlink);
 }
 
 our sub tempdir (
     $tmpl? = '*' x 10,          # positional template
     :$tempdir? = $*TMPDIR,      # where to create tempdir
-    :$prefix? = '',             # dirname prefix
+    :$prefix? = '',             # directory prefix
+    :$suffix? = '',             # directory suffix
     :$unlink?  = 1,             # remove when program exits?
     :$template = $tmpl          # required named template
 ) is export {
-
-    my $count = MAX-RETRIES;
-    while ($count--) {
-        my $tempdir_name = $template;
-        $tempdir_name ~~ s/ '*' ** 4..* /{ gen-random($/.chars) }/;
-        my $dirpath = "$tempdir/$prefix$tempdir_name";
-        next if $dirpath.IO ~~ :e;
-        try { CATCH { next }; mkdir($dirpath) };
-        push @open-files, $dirpath if $unlink;
-        return $dirpath;
-    }
-    return ();
+    return make-temp('dir', $template, $tempdir, $prefix, $suffix, $unlink);
 }
 
 END {
