d58f8026ce5738e9c39cb7c62c5215b8a65d0bd7
Moritz Lenz <moritz@faui2k3.org>
2015-11-24 20:56:18 +0000

    git subrepo clone --force git://github.com/perl6/doc.git perl6/doc
    subrepo:
      subdir:   "perl6/doc"
      merged:   "9e567b0"
    upstream:
      origin:   "git://github.com/perl6/doc.git"
      branch:   "master"
      commit:   "9e567b0"
    git-subrepo:
      version:  "0.2.2"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "436d7b7"

--- perl6/doc/html/js/main.js
+++ perl6/doc/html/js/main.js
@@ -7,6 +7,7 @@ function setup_search_box() {
     var sel = $('#search'), head = $('#logo').parent();
     if ( ! sel.length ) { return; }
 
+    /* Setup handling of narrow screens */
     if ( head.offset().top + head.innerHeight() <= sel.offset().top ) {
         sel.addClass('two-row');
     }
@@ -14,5 +15,14 @@ function setup_search_box() {
         sel.removeClass('two-row');
     }
 
-    $('#query').focus();
+    /* Focus search box on page load, but remove focus if the user appears
+        to be trying to scroll the page with keyboard, rather than typing
+        a search query
+    */
+    $('#query').keydown( function(e){
+        var el = $(this);
+        if ( e.which == 32 && el.val().length                ) { return true; }
+        if ( e.which == 32 || e.which == 34 || e.which == 40 ) { el.blur()  ; }
+        // key codes: 32: space; 34: pagedown; 40: down arrow
+    });
 }
