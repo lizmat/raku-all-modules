2ca2230b0c4e9bea860154361620db75d2c25222
Moritz Lenz <moritz@faui2k3.org>
2016-01-14 10:50:47 +0000

    git subrepo clone --force git://github.com/vendethiel/Serialize-Tiny.pm6.git vendethiel/Serialize-Tiny.pm6
    subrepo:
      subdir:   "vendethiel/Serialize-Tiny.pm6"
      merged:   "f80734a"
    upstream:
      origin:   "git://github.com/vendethiel/Serialize-Tiny.pm6.git"
      branch:   "master"
      commit:   "f80734a"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

--- vendethiel/Serialize-Tiny.pm6/lib/Serialize/Tiny.pm
+++ vendethiel/Serialize-Tiny.pm6/lib/Serialize/Tiny.pm
@@ -2,6 +2,6 @@ unit module Serialize::Tiny;
 
 sub serialize(Mu:D \obj) is export {
   my \type = obj.WHAT;
-  my @attribute = type.^attributes.grep(*.has-accessor);
-  @attribute.map({.name.substr(2), .get_value(obj)}).hash
+  my @attribute = type.^attributes.grep(*.has_accessor);
+  @attribute.map({.name.substr(2) => .get_value(obj)}).hash
 }
