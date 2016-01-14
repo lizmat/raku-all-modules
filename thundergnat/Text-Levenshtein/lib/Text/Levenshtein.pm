fed25d5dd0b5b7299fa2524f9d554357d8f2d32d
Moritz Lenz <moritz@faui2k3.org>
2016-01-14 10:33:20 +0000

    git subrepo clone --force git://github.com/thundergnat/Text-Levenshtein.git thundergnat/Text-Levenshtein
    subrepo:
      subdir:   "thundergnat/Text-Levenshtein"
      merged:   "b8b206c"
    upstream:
      origin:   "git://github.com/thundergnat/Text-Levenshtein.git"
      branch:   "master"
      commit:   "b8b206c"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

--- thundergnat/Text-Levenshtein/lib/Text/Levenshtein.pm
+++ thundergnat/Text-Levenshtein/lib/Text/Levenshtein.pm
@@ -1,50 +1,30 @@
-unit module Text::Levenshtein;
+unit module Text::Levenshtein:ver<0.2.0>:auth<github:thundergnat>;
 
-sub _min 
-{
-	return $^a < $^b
-		?? $^a < $^c ?? $^a !! $^c
-		!! $^b < $^c ?? $^b !! $^c;
-}
-
-
-sub distance ($s, *@t) is export
-{
+sub distance ($s, *@t) is export {
 
 	my $n = $s.chars;
 	my @result;
 
-	for (@t) -> $t {
-		if ($s eq $t) {
-			push @result, 0;
-			next;
-		}
-		my @d;
-		my $cost=0;
-
-		my $m = $t.chars;
+	for @t -> $t {
+		@result.push(0)  and next if $s eq $t;
+        @result.push($n) and next unless my $m = $t.chars;
 		@result.push($m) and next unless $n;
-		@result.push($n) and next unless $m;
 
-		@d[0][0] = 0;
-		for (1 .. $n) -> $i {
-			@d[$i][0] = $i;
-		}
-		for (1 .. $m) -> $j {
-			@d[0][$j] = $j;
-		}
+		my @d;
 
-		for (1 .. $n) -> $i {
-			my $s_i = $s.substr($i-1,1);
-			for (1 .. $m) -> $j {
-				@d[$i][$j] = 
-				  _min(@d[$i-1][$j]+1, @d[$i][$j-1]+1,
-					 @d[$i-1][$j-1]+($s_i eq substr($t,$j-1,1) ?? 0 !! 1) )
+		map { @d[$_; 0] = $_ }, 1 .. $n;
+		map { @d[0; $_] = $_ }, 0 .. $m;
+
+		for 1 .. $n -> $i {
+			my $s_i = $s.substr($i-1, 1);
+			for 1 .. $m -> $j {
+				@d[$i; $j] = min @d[$i-1; $j] + 1, @d[$i; $j-1] + 1,
+				  @d[$i-1; $j-1] + ($s_i eq $t.substr($j-1, 1) ?? 0 !! 1)
 			}
 		}
-		push @result,@d[$n][$m];
+		@result.push: @d[$n; $m];
 	}
-	return @result;
+	return |@result;
 }
 
 
@@ -65,32 +45,24 @@ Text::Levenshtein - An implementation of the Levenshtein edit distance
 
  print "@distances";
  # prints "2 0 3"
- 
+
 
 =head1 DESCRIPTION
 
 This module implements the Levenshtein edit distance.
 The Levenshtein edit distance is a measure of the degree of proximity between two strings.
-This distance is the number of substitutions, deletions or insertions ("edits") 
+This distance is the number of substitutions, deletions or insertions ("edits")
 needed to transform one string into the other one (and vice versa).
 When two strings have distance 0, they are the same.
 A good point to start is: <http://www.merriampark.com/ld.htm>
 
-#See also Text::LevenshteinXS on CPAN if you do not require a perl-only implementation.  It
-#is extremely faster in nearly all cases.
-
-#See also Text::WagnerFischer on CPAN for a configurable edit distance, i.e. for
-#configurable costs (weights) for the edits.
-
-
 =head1 AUTHOR
 
 Copyright 2002 Dree Mistrut <F<dree@friul.it>>
-perl6 port: 2010 Steve Schulze 
+perl6 port: 2010 Steve Schulze
 
 This package is free software and is provided "as is" without express
-or implied warranty.  You can redistribute it and/or modify it under 
+or implied warranty.  You can redistribute it and/or modify it under
 the same terms as Perl itself.
 
 =end pod
-
--- thundergnat/Text-Levenshtein/t/basic.t
+++ thundergnat/Text-Levenshtein/t/basic.t
@@ -1,10 +1,13 @@
 use Test;
-plan 9;
-
 use Text::Levenshtein;
+
+plan 12;
 
 is(distance("foo","four"),2,"Correct distance foo four");
 is(distance("foo","foo"),0,"Correct distance foo foo");
+is(distance("","foo"),3,"Correct distance '' foo");
+is(distance("foo",""),3,"Correct distance foo ''");
+is(distance(1, 12),1,"Correct distance 1 12");
 is(distance("cow","cat"),2,"Correct distance cow cat");
 is(distance("cat","moocow"),5,"Correct distance cat moocow");
 is(distance("cat","cowmoo"),5,"Correct distance cat cowmoo");
@@ -15,4 +18,4 @@ my @foo = distance("foo","four","foo","bar");
 my @bar = (2,0,3);
 is(@foo,@bar,"Array test: Correct distances foo four foo bar");
 
-
+done-testing();
