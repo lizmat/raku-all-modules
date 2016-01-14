3920f9c7a2f5e0e3b5491167ab4e237ff727a876
Moritz Lenz <moritz@faui2k3.org>
2016-01-14 10:29:09 +0000

    git subrepo clone --force git://github.com/sergot/IO-Capture-Simple.git sergot/IO-Capture-Simple
    subrepo:
      subdir:   "sergot/IO-Capture-Simple"
      merged:   "baf8dcf"
    upstream:
      origin:   "git://github.com/sergot/IO-Capture-Simple.git"
      branch:   "master"
      commit:   "baf8dcf"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

--- sergot/IO-Capture-Simple/lib/Test/IO/Capture.pm
+++ sergot/IO-Capture-Simple/lib/Test/IO/Capture.pm
@@ -3,13 +3,13 @@ use IO::Capture::Simple;
 
 unit module Test::IO::Capture;
 
-sub prints-stdout-ok (Callable $code, $expected as Str, $reason = '')
+sub prints-stdout-ok (Callable $code, Str() $expected, $reason = '')
 is export {
     my $out = capture_stdout $code;
     is $out, $expected, $reason;
 }
 
-sub prints-stderr-ok (Callable $code, $expected as Str, $reason = '')
+sub prints-stderr-ok (Callable $code, Str() $expected, $reason = '')
 is export {
     my $out = capture_stderr $code;
     is $out, $expected, $reason;
