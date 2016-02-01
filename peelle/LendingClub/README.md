d3529547881348eaa7dafc94476d4f84288346f0
Moritz Lenz <moritz@faui2k3.org>
2016-02-01 16:11:28 +0000

    git subrepo clone --force git://github.com/peelle/LendingClub.git peelle/LendingClub
    subrepo:
      subdir:   "peelle/LendingClub"
      merged:   "54591da"
    upstream:
      origin:   "git://github.com/peelle/LendingClub.git"
      branch:   "master"
      commit:   "54591da"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

---
+++ peelle/LendingClub/README.md
@@ -0,0 +1,101 @@
+﻿LendingClub
+==========
+
+[Lending Club API](https://www.lendingclub.com/developers/lc-api.action). I wrote this to further my [plan to retire young](https://github.com/peelle/Finance-CompoundInterest) on a beach somewhere.
+
+SYNOPSIS
+===========
+
+A wrapper for the Lending Club API. It wraps all of the functions they currently provide. In the below examples, I modified the return #'s for my example query. 
+
+EXAMPLE
+=======
+```
+use LendingClub;
+use Data::Dump;
+
+my $lc = LendingClub.new(token => 'somesecrettoken', accountId => 12345 ); 
+
+say Dump $lc.summary;'
+
+```
+
+{
+  accountTotal         => 1000000000.00.Rat,
+  accruedInterest      => 1.00.Rat,
+  availableCash        => 100.00.Rat,
+  infundingBalance     => 100.Int,
+  investorId           => 12345.Int,
+  outstandingPrincipal => 100000000.Rat,
+  receivedInterest     => 1.00.Rat,
+  receivedLateFees     => 0.Int,
+  receivedPrincipal    => 10.00.Rat,
+  totalNotes           => 111.Int,
+  totalPortfolios      => 5.Int,
+}
+
+```
+my $dt = DateTime.new( year => 2016, month => 3, day => 3); 
+my $transfer = $lc.transferFunds( "LOAD_ONCE", 50.00, $dt.Str ); ';
+
+$lc.cancelTransfers( [ $transfer<transfers>[0]<transferId> ] );
+
+my $avail_notes = $lc.listing;
+
+.... Some code to decide what notes to invest in from the list.....
+
+$lc.submitOrders( 12345, @some_notes_i_want );
+
+
+```
+
+INSTALLATION
+============
+     > panda install LendingClub
+
+DEPENDANCIES
+============
+* [JSON::Tiny](https://github.com/moritz/json)
+* [Net::HTTP](https://github.com/ugexe/Perl6-Net--HTTP)
+
+METHODS
+=======
+> Queries for info.
+
+* summary
+* availableCash
+* pending
+* notes
+* detailedNotes
+* portfolios
+* listing( Bool $showAll = False )
+
+> Actions that affect your account.
+
+* transerFunds( Str $transferFrequency, Rat() $amount, Str $startDate?, Str $endDate?, )
+* cancelTransfers( @transferIds )
+* createPortfolio( Int $aid, Str $portfolioName, Str $portfolioDescription? )
+* submitOrders( Int $aid, @orders )
+** aid is your account number. 
+
+BUGS
+====
+
+* I haven't tested it as throughly as I wanted. Specifically I don't have multiple account types(investing, and retirement) so  I don't know if it works right for people with both.
+* Setting listing to true attempts to get ALL notes on the Lending Club platform. Using a true value for the listing method crashes on my system. I haven't looked into why.
+
+
+TODO/HELP PLEASE
+====
+* More/better tests.
+
+AUTHOR
+======
+
+James (Jeremy) Carman <developer@peelle.org>
+
+ACKNOWLEDGEMENTS
+================
+
+* Mad thanks to [ugexe](https://github.com/ugexe) for accepting my pull request. Without his awesome [Net::HTTP::*](https://github.com/ugexe/Perl6-Net--HTTP) modules, I would have given up on this module. It was the third module of this type I tried.
+
---
+++ peelle/LendingClub/lib/LendingClub.pm
@@ -0,0 +1,93 @@
+class LendingClub {
+
+	has Str $.token;
+	has Int $.accountId is rw;
+	has Str $.version = 'v1';
+	has Str $.uri = "https://api.lendingclub.com/api/investor/$!version/accounts/$!accountId/";
+
+	use JSON::Tiny;
+	use Net::HTTP::GET;
+	use Net::HTTP::POST;
+
+	method !request (Str $type where { $type eq 'GET'|'POST' }, Str $api_call, %body? ) returns Hash {
+		my $uri = self.uri ~ $api_call;
+		my %header = ( Accept => 'application/json', Authorization => $!token );
+		my $results;
+
+		if $type eq 'GET' {
+			$uri ~~ s/accounts.*/loans\/$api_call/ if $api_call ~~ /listing/; # A quick hack for dealing with the one not accounts method.
+			$results = Net::HTTP::GET( $uri, :%header );
+		} else {
+			%header<Content-type> = 'application/json'; # Only needed for Posts
+			my $body = Buf.new(  (to-json( %body ).ords) );
+
+			$results = Net::HTTP::POST( $uri, :%header, :$body );
+		}
+
+		given $results.status-code {
+			when 200 { from-json $results.content :force }
+			when 400 { { error => 'Execution failed with errors. Errors will be returned as JSON payload.', message => from-json $results.content :force } }
+			when 403 { { error => 'Authentication failure.' } }
+			when 404 { { error => 'Resource does not exist.' } }
+			when 500 { { error => 'Unsuccessful execution.' } }
+			default  { { error => 'Unkown error.', status-code => $results.status-code } }
+		}
+	}
+
+	### GET REQUESTS ###
+
+	method summary {
+		return self!request('GET', 'summary');
+	}
+
+	method availableCash {
+		return self!request('GET', 'availablecash');
+	}
+
+	method pending {
+		return self!request('GET', 'funds/pending');
+	}
+
+	
+	method notes {
+		return self!request('GET', 'notes');
+	}
+
+	method detailedNotes {
+		return self!request('GET', 'detailednotes');
+	}
+
+	method portfolios {
+		return self!request('GET', 'portfolios');
+	}
+
+	method listing (Bool $showAll = False) {
+		return self!request('GET', 'listing?showAll=true') if $showAll; # Currently crashes after 4+ minutes of I'm guess is weating or downloading.
+
+		return self!request('GET', 'listing');
+	}
+
+	### POST REQUESTS ###
+
+	method transferFunds ( 
+			Str$transferFrequency where { $transferFrequency eq  'LOAD_NOW'|'LOAD_ONCE'|'LOAD_WEEKLY'|'LOAD_BIWEEKLY'|'LOAD_ON_DAY_1_AND_16'|'LOAD_MONTHLY'},
+			Rat() $amount, 
+			Str $startDate?,
+			Str $endDate?,
+	) {
+		return self!request('POST', 'funds/add', { :$transferFrequency, :$amount, :$startDate, :$endDate } );
+	}
+
+	method cancelTransfers ( @transferIds ) {
+		return self!request('POST', 'funds/cancel', { :@transferIds } );
+	}
+
+	method createPortfolio ( Int $aid, Str $portfolioName, Str $portfolioDescription?) {
+			return self!request('POST', 'portfolios', { :$aid, :$portfolioName, :$portfolioDescription } );
+	}
+
+	method submitOrders ( Int $aid,  @orders ) {
+		return self!request('POST', 'orders', { :$aid, :@orders } );
+	}
+			
+}
