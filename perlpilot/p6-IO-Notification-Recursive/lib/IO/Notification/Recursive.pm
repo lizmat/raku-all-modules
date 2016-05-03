e05ef110214654c1b64f938ee5d8875b217c7861
Moritz Lenz <moritz@faui2k3.org>
2016-05-03 11:36:36 +0000

    git subrepo clone --force git://github.com/perlpilot/p6-IO-Notification-Recursive.git perlpilot/p6-IO-Notification-Recursive
    subrepo:
      subdir:   "perlpilot/p6-IO-Notification-Recursive"
      merged:   "2bd7966"
    upstream:
      origin:   "git://github.com/perlpilot/p6-IO-Notification-Recursive.git"
      branch:   "master"
      commit:   "2bd7966"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

--- perlpilot/p6-IO-Notification-Recursive/lib/IO/Notification/Recursive.pm
+++ perlpilot/p6-IO-Notification-Recursive/lib/IO/Notification/Recursive.pm
@@ -3,18 +3,19 @@ unit module IO::Notification::Recursive;
 use File::Find;
 
 sub watch-recursive(Str $path, Bool :$update) is export {
-    my @dirs := find(:dir($path), :type<dir>);      # Get all subdirectories so we can watch them too.
-    my @paths := [ $path,  flat @dirs.map({ ~$_ }) ];
     supply {
         my sub watch-it($p) {
             whenever IO::Notification.watch-path($p) -> $e {
                 if $update && $e.event ~~ FileRenamed && $e.path.IO ~~ :d {
-                    watch-it($e.path);
+                    watch-it($_) for find-dirs $e.path;
                 }
-                supply-emit($e);
+                emit($e);
             }
         }
-        watch-it($_) for @paths;
+        watch-it(~$_) for find-dirs $path;
     }
 }
 
+my sub find-dirs (Str:D $p) {
+    return slip $p.IO, slip find :dir($p), :type<dir>;
+}
