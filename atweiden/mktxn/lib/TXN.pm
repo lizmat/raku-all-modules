079593f0135655edf915c9ae6f61de6d8d9aa9ea
Moritz Lenz <moritz@faui2k3.org>
2016-09-13 06:58:02 +0000

    git subrepo clone --force git://github.com/atweiden/mktxn.git atweiden/mktxn
    subrepo:
      subdir:   "atweiden/mktxn"
      merged:   "de3f2a4"
    upstream:
      origin:   "git://github.com/atweiden/mktxn.git"
      branch:   "master"
      commit:   "de3f2a4"
    git-subrepo:
      version:  "0.2.3"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "87a8275"

--- atweiden/mktxn/lib/TXN.pm
+++ atweiden/mktxn/lib/TXN.pm
@@ -7,7 +7,7 @@ use TXN::Remarshal;
 unit module TXN;
 
 constant $PROGRAM = 'mktxn';
-constant $VERSION = v0.0.5;
+constant $VERSION = v0.0.6;
 
 # TXN::Package {{{
 
@@ -29,7 +29,7 @@ my class TXN::Package
 
     # package info
     has Str $!pkgname is required;
-    has Str $!pkgver is required;
+    has Version $!pkgver is required;
     has UInt $!pkgrel is required;
     has Str $!pkgdesc;
 
@@ -38,54 +38,87 @@ my class TXN::Package
     # --- submethod BUILD {{{
 
     submethod BUILD(
-        Str :$content!,
-        :%prepare! (
-            Str :$!pkgname!,
-            Str :$!pkgver!,
-            UInt :$!pkgrel!,
-            Str :$pkgdesc,
-            Int :$date-local-offset,
-            Str :$txn-dir
-        ),
-        Bool :$verbose
+        Str :$!compiler!,
+        TXN::Parser::AST::Entry :@!entry!,
+        UInt :$!count!,
+        # XXX C<@entities-seen> param is typed Str to make compiler happy
+        Str :@!entities-seen!,
+        Str :$!pkgname!,
+        Version :$!pkgver!,
+        UInt :$!pkgrel!,
+        Str :$pkgdesc
     )
     {
         $!pkgdesc = $pkgdesc if $pkgdesc;
-
-        my DateTime $dt = now.DateTime;
-        say "Making txn pkg: $!pkgname $!pkgver-$!pkgrel ($dt)" if $verbose;
-        $!compiler = $PROGRAM ~ ' v' ~ $VERSION ~ " $dt";
-
-        # parse the accounting ledger
-        my %h;
-        %h<date-local-offset> = $date-local-offset if $date-local-offset;
-        %h<txn-dir> = $txn-dir if $txn-dir;
-        @!entry = from-txn($content, |%h);
-
-        # compute basic stats about the accounting ledger
-        $!count = @!entry.elems;
-        @!entities-seen = get-entities-seen(@!entry);
     }
 
     # --- end submethod BUILD }}}
     # --- method new {{{
 
     method new(
-        Str $content,
+        # whether C<$cf> is content of a file, or is a file path
+        #
+        # if file path, we pass as arg C<:file> to C<from-txn> to get
+        # proper include directive handling
+        Str $content-or-file where /CONTENT|FILE/,
+
+        # the content of a file, or file path
+        Str $cf,
+
+        # whether to print console progress messages
         Bool :$verbose = False,
+
         *%opts (
-            Str :$pkgname,
-            Str :$pkgver,
-            UInt :$pkgrel,
-            Str :$pkgdesc,
-            Int :$date-local-offset,
-            Str :$txn-dir,
-            Str :$template
+            Str :pkgname($),
+            Str :pkgver($),
+            UInt :pkgrel($),
+            Str :pkgdesc($),
+            Int :date-local-offset($),
+            Str :txn-dir($),
+            Str :template($)
         )
     )
     {
+        my %bless;
+
         my %prepare = prepare(|%opts);
-        self.bless(:$content, :%prepare, :$verbose);
+
+        my Str $pkgname = %prepare<pkgname>;
+        my Version $pkgver .= new(%prepare<pkgver>);
+        my UInt $pkgrel = %prepare<pkgrel>;
+        my Str $pkgdesc = %prepare<pkgdesc> if %prepare<pkgdesc>;
+
+        my DateTime $dt = now.DateTime;
+
+        say "Making txn pkg: $pkgname $pkgver-$pkgrel ($dt)" if $verbose;
+
+        my Str $compiler = "$PROGRAM v$VERSION $dt";
+
+        # parse the accounting ledger
+        my %h;
+        %h<date-local-offset> =
+            %prepare<date-local-offset> if %prepare<date-local-offset>;
+        %h<txn-dir> = %prepare<txn-dir> if %prepare<txn-dir>;
+        my TXN::Parser::AST::Entry @entry = do given $content-or-file
+        {
+            when 'CONTENT' { from-txn($cf, |%h) }
+            when 'FILE' { from-txn(:file($cf), |%h) }
+        }
+
+        # compute basic stats about the accounting ledger
+        my UInt $count = @entry.elems;
+        my VarName @entities-seen = get-entities-seen(@entry);
+
+        %bless<compiler> = $compiler;
+        %bless<entry> = @entry;
+        %bless<count> = $count;
+        %bless<entities-seen> = @entities-seen;
+        %bless<pkgname> = $pkgname;
+        %bless<pkgver> = $pkgver;
+        %bless<pkgrel> = $pkgrel;
+        %bless<pkgdesc> = $pkgdesc if $pkgdesc;
+
+        self.bless(|%bless);
     }
 
     # --- end method new }}}
@@ -234,7 +267,26 @@ multi sub mktxn(
 ) is export
 {
     my Str $f = resolve-txn-file-path($file);
-    my %txn-package = TXN::Package.new(slurp($f), :verbose, |%opts).hash;
+    my %txn-package = TXN::Package.new('FILE', $f, :verbose, |%opts).hash;
+    package(%txn-package);
+}
+
+multi sub mktxn(
+    Str $content,
+    Bool :$release! where *.so,
+    *%opts (
+        Str :$pkgname,
+        Str :$pkgver,
+        UInt :$pkgrel,
+        Str :$pkgdesc,
+        Str :$txn-dir,
+        Int :$date-local-offset,
+        Str :$template
+    )
+) is export
+{
+    my %txn-package =
+        TXN::Package.new('CONTENT', $content, :verbose, |%opts).hash;
     package(%txn-package);
 }
 
@@ -251,7 +303,7 @@ multi sub mktxn(
     )
 ) is export returns Hash
 {
-    mktxn(slurp($file), |%opts);
+    TXN::Package.new('FILE', $file, |%opts).hash;
 }
 
 multi sub mktxn(
@@ -267,7 +319,7 @@ multi sub mktxn(
     )
 ) is export returns Hash
 {
-    TXN::Package.new($content, |%opts).hash;
+    TXN::Package.new('CONTENT', $content, |%opts).hash;
 }
 
 # end sub mktxn }}}
