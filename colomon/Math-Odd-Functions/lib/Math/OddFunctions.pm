a01aa25782264d4562d62224141df85e1cc7ba44
Moritz Lenz <moritz@faui2k3.org>
2017-01-21 11:27:50 +0000

    git subrepo clone --force git://github.com/colomon/Math-Odd-Functions.git colomon/Math-Odd-Functions
    subrepo:
      subdir:   "colomon/Math-Odd-Functions"
      merged:   "2d80bdc"
    upstream:
      origin:   "git://github.com/colomon/Math-Odd-Functions.git"
      branch:   "master"
      commit:   "2d80bdc"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- colomon/Math-Odd-Functions/lib/Math/OddFunctions.pm
+++ colomon/Math-Odd-Functions/lib/Math/OddFunctions.pm
@@ -3,30 +3,30 @@ use NativeCall;
 
 # libSystem should maybe be libm on Linux?
 
-sub erf(num) returns num is native(Str)  { * }
-sub error-function(Real $a) is export { erf(num.new($a.Num)) }
+sub erf(num64) returns num64 is native(Str)  { * }
+sub error-function(Real $a) is export { erf($a.Num) }
 
-sub erfc(num) returns num is native(Str)  { * }
-sub complementary-error-function(Real $a) is export { erfc(num.new($a.Num)) }
+sub erfc(num64) returns num64 is native(Str)  { * }
+sub complementary-error-function(Real $a) is export { erfc($a.Num) }
 
-sub tgamma(num) returns num is native(Str)  { * }
-multi Γ(Real $a) is export { $a == 0 ?? NaN !! tgamma(num.new($a.Num)) }
+sub tgamma(num64) returns num64 is native(Str)  { * }
+multi Γ(Real $a) is export { $a == 0 ?? NaN !! tgamma($a.Num) }
 multi Γ(Int $a) is export {
     given $a {
         when * < 1 { NaN }
-        when * < 13 { tgamma(num.new($a.Num)).round }
+        when * < 13 { tgamma($a.Num).round }
         default { [*] 1..($a - 1) }
     }
 }
 
-sub lgamma(num) returns num is native(Str)  { * }
-multi logΓ(Real $a) is export { $a == 0 ?? NaN !! lgamma(num.new($a.Num)) }
+sub lgamma(num64) returns num64 is native(Str)  { * }
+multi logΓ(Real $a) is export { $a == 0 ?? NaN !! lgamma($a.Num) }
 
-sub log1p-num(num) returns num is native(Str) is symbol('log1p')  { * }
-multi log1p(Real $a) is export { log1p-num(num.new($a.Num)) }
+sub log1p-num(num64) returns num64 is native(Str) is symbol('log1p')  { * }
+multi log1p(Real $a) is export { log1p-num($a.Num) }
 
-sub expm1-num(num) returns num is native(Str) is symbol('expm1')  { * }
-multi expm1(Real $a) is export { expm1-num(num.new($a.Num)) }
+sub expm1-num(num64) returns num64 is native(Str) is symbol('expm1')  { * }
+multi expm1(Real $a) is export { expm1-num($a.Num) }
 
 # sub expm1(Real $a) is export { $a.abs < 1e-5 ?? $a + 0.5*$a*$a !! $a.exp - 1 }
 
