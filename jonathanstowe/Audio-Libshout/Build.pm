4c6e938b3a53e9bb6fa73cb444cf3f4217a937e5
Moritz Lenz <moritz@faui2k3.org>
2017-01-28 17:30:49 +0000

    git subrepo clone --force git://github.com/jonathanstowe/Audio-Libshout.git jonathanstowe/Audio-Libshout
    subrepo:
      subdir:   "jonathanstowe/Audio-Libshout"
      merged:   "531ed9c"
    upstream:
      origin:   "git://github.com/jonathanstowe/Audio-Libshout.git"
      branch:   "master"
      commit:   "531ed9c"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- jonathanstowe/Audio-Libshout/Build.pm
+++ jonathanstowe/Audio-Libshout/Build.pm
@@ -1,15 +1,17 @@
-use v6;
+use v6.c;
 
-use Panda::Common;
-use Panda::Builder;
 use LibraryCheck;
 
-class Build is Panda::Builder {
+class Build {
     method build($workdir) {
         if !library-exists('shout', v3) {
             say "Won't build because no libshout (API v3)";
             die "You need to have libshout installed";
         }
         True;
+    }
+    method isa($what) {
+        return True if $what.^name eq 'Panda::Builder';
+        callsame;
     }
 }
