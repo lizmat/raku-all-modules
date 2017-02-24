5141c2d03c38abec88839b57fe738a2d3dc3684b
Moritz Lenz <moritz@faui2k3.org>
2017-02-24 20:24:11 +0000

    git subrepo clone --force git://github.com/timo/json_fast.git timo/json_fast
    subrepo:
      subdir:   "timo/json_fast"
      merged:   "55a5a37"
    upstream:
      origin:   "git://github.com/timo/json_fast.git"
      branch:   "master"
      commit:   "55a5a37"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- timo/json_fast/lib/JSON/Fast.pm
+++ timo/json_fast/lib/JSON/Fast.pm
@@ -13,7 +13,20 @@ sub str-escape(str $text) {
 
 sub to-json($obj is copy, Bool :$pretty = True, Int :$level = 0, Int :$spacing = 2) is export {
     return $obj ?? 'true' !! 'false' if $obj ~~ Bool;
-    return $obj.Str if $obj ~~ Int|Rat|Num;
+    return $obj.Str if $obj ~~ Int|Rat;
+
+    if $obj ~~ Num {
+        if $obj === NaN || $obj === -Inf || $obj === Inf {
+            if try $*JSON_NAN_INF_SUPPORT {
+                return $obj.Str;
+            } else {
+                return "null";
+            }
+        } else {
+            return $obj.Str;
+        }
+    }
+
     return 'null' if not $obj.defined;
     return "\"{str-escape($obj)}\"" if $obj ~~ Str;
 
