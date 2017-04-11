986739380cf88c5d5f7fe4860e00fd350678a746
Moritz Lenz <moritz@faui2k3.org>
2017-04-11 16:48:18 +0000

    git subrepo clone --force git://github.com/atweiden/digest-xxhash.git atweiden/digest-xxhash
    subrepo:
      subdir:   "atweiden/digest-xxhash"
      merged:   "4ab4044"
    upstream:
      origin:   "git://github.com/atweiden/digest-xxhash.git"
      branch:   "master"
      commit:   "4ab4044"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- atweiden/digest-xxhash/lib/Digest/xxHash.pm
+++ atweiden/digest-xxhash/lib/Digest/xxHash.pm
@@ -5,33 +5,33 @@ unit module Digest::xxHash;
 # xxHash C wrapper functions (/usr/lib/libxxhash.so) {{{
 
 # unsigned int XXH32 (const void* input, size_t length, unsigned seed);
-sub XXH32(CArray[int8], size_t, uint32) returns uint32 is native('xxhash', v0.42.0) {*}
+sub XXH32(CArray[int8], size_t, uint32 --> uint32) is native('xxhash', v0.42.0) {*}
 
 # unsigned long long XXH64 (const void* input, size_t length, unsigned long long seed);
-sub XXH64(CArray[int8], size_t, ulonglong) returns ulonglong is native('xxhash', v0.42.0) {*}
+sub XXH64(CArray[int8], size_t, ulonglong --> ulonglong) is native('xxhash', v0.42.0) {*}
 
 # end xxHash C wrapper functions (/usr/lib/libxxhash.so) }}}
 
 # 32 or 64 bit {{{
 
-multi sub xxHash(Str $string, Int :$seed = 0) is export returns Int
+multi sub xxHash(Str $string, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $string.split('')».encode».contents.flat».Int;
     build-xxhash(@data, $seed);
 }
 
-multi sub xxHash(Str :$file!, Int :$seed = 0) is export returns Int
+multi sub xxHash(Str :$file!, Int :$seed = 0 --> Int) is export
 {
     xxHash(slurp($file), :$seed);
 }
 
-multi sub xxHash(Buf[uint8] :$buf-u8!, Int :$seed = 0) is export returns Int
+multi sub xxHash(Buf[uint8] :$buf-u8!, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $buf-u8.list;
     build-xxhash(@data, $seed);
 }
 
-sub build-xxhash(Int @data, Int $seed = 0) returns Int
+sub build-xxhash(Int @data, Int $seed = 0 --> Int)
 {
     $*KERNEL.bits == 64
         ?? build-xxhash64(@data, $seed)
@@ -41,24 +41,24 @@ sub build-xxhash(Int @data, Int $seed = 0) returns Int
 # end 32 or 64 bit }}}
 # 32 bit only {{{
 
-multi sub xxHash32(Str $string, Int :$seed = 0) is export returns Int
+multi sub xxHash32(Str $string, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $string.split('')».encode».contents.flat».Int;
     build-xxhash32(@data, $seed);
 }
 
-multi sub xxHash32(Str :$file!, Int :$seed = 0) is export returns Int
+multi sub xxHash32(Str :$file!, Int :$seed = 0 --> Int) is export
 {
     xxHash32(slurp($file), :$seed);
 }
 
-multi sub xxHash32(Buf[uint8] :$buf-u8!, Int :$seed = 0) is export returns Int
+multi sub xxHash32(Buf[uint8] :$buf-u8!, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $buf-u8.list;
     build-xxhash32(@data, $seed);
 }
 
-sub build-xxhash32(Int @data, uint $seed = 0) returns uint
+sub build-xxhash32(Int @data, uint $seed = 0 --> uint)
 {
     my @input := CArray[int8].new;
     my Int $len = 0;
@@ -69,24 +69,24 @@ sub build-xxhash32(Int @data, uint $seed = 0) returns uint
 # end 32 bit only }}}
 # 64 bit only {{{
 
-multi sub xxHash64(Str $string, Int :$seed = 0) is export returns Int
+multi sub xxHash64(Str $string, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $string.split('')».encode».contents.flat».Int;
     build-xxhash64(@data, $seed);
 }
 
-multi sub xxHash64(Str :$file!, Int :$seed = 0) is export returns Int
+multi sub xxHash64(Str :$file!, Int :$seed = 0 --> Int) is export
 {
     xxHash64(slurp($file), :$seed);
 }
 
-multi sub xxHash64(Buf[uint8] :$buf-u8!, Int :$seed = 0) is export returns Int
+multi sub xxHash64(Buf[uint8] :$buf-u8!, Int :$seed = 0 --> Int) is export
 {
     my Int @data = $buf-u8.list;
     build-xxhash64(@data, $seed);
 }
 
-sub build-xxhash64(Int @data, ulonglong $seed = 0) returns ulonglong
+sub build-xxhash64(Int @data, ulonglong $seed = 0 --> ulonglong)
 {
     my @input := CArray[int8].new;
     my Int $len = 0;
