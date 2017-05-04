15c08e88f9a066146c781070f8642cb53e6469a2
Moritz Lenz <moritz@faui2k3.org>
2017-05-04 05:18:04 +0000

    Remove repos that no longer exist
    (This commit was automatically generated)

--- kuerbis/Term-Form-p6/t/01-basic.t
+++
@@ -1,9 +0,0 @@
-use v6;
-use Test;
-plan 1;
-
-use Term::Form;
-
-pass "use Term::Form;";
-
-done-testing();
--- p6-pdf/perl6-PDF-Tools/lib/PDF/Storage/Input/Str.pm
+++
@@ -1,7 +0,0 @@
-use v6;
-use PDF::Storage::Input;
-
-class PDF::Storage::Input::Str
-    is PDF::Storage::Input
-    is Str {
-}
