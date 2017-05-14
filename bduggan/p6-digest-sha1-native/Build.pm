ee246391c161e6b521d22020b022fd2b2578e0f6
Moritz Lenz <moritz@faui2k3.org>
2017-05-14 05:13:18 +0000

    git subrepo clone (merge) --force git://github.com/bduggan/p6-digest-sha1-native.git bduggan/p6-digest-sha1-native
    subrepo:
      subdir:   "bduggan/p6-digest-sha1-native"
      merged:   "4f6536c"
    upstream:
      origin:   "git://github.com/bduggan/p6-digest-sha1-native.git"
      branch:   "master"
      commit:   "4f6536c"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- bduggan/p6-digest-sha1-native/Build.pm
+++ bduggan/p6-digest-sha1-native/Build.pm
@@ -1,10 +1,8 @@
 use v6;
-use Panda::Common;
-use Panda::Builder;
 use LibraryMake;
 use Shell::Command;
 
-class Build is Panda::Builder {
+class Build {
     method build($dir) {
         my %vars = get-vars($dir);
         %vars<sha1> = $*VM.platform-library-name('sha1'.IO);
