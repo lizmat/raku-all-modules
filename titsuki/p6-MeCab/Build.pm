2190ba03948cdccff2d55343ae47e591882c8a50
Moritz Lenz <moritz@faui2k3.org>
2017-07-21 08:28:58 +0000

    git subrepo clone (merge) --force git://github.com/titsuki/p6-MeCab.git titsuki/p6-MeCab
    subrepo:
      subdir:   "titsuki/p6-MeCab"
      merged:   "440e4e0"
    upstream:
      origin:   "git://github.com/titsuki/p6-MeCab.git"
      branch:   "master"
      commit:   "440e4e0"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- titsuki/p6-MeCab/Build.pm
+++ titsuki/p6-MeCab/Build.pm
@@ -1,4 +1,6 @@
 use LibraryMake;
+use Zef::Fetch;
+use Zef::Extract;
 
 class Build {
     method build($workdir) {
@@ -25,21 +27,23 @@ class Build {
         my $goback = $*CWD;
         my $srcdir = "$workdir/src";
         my %vars = get-vars($workdir);
-        chdir($srcdir);
-        my $wget-exit-code = run 'which', 'wget';
-        if $wget-exit-code == 0 {
-            my $CMD = qq:x/which wget/.subst(/\s*/,"",:g);
-            my $url = "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE";
-            my $dl-exit-code = run $CMD, $url, "-O", "mecab-0.996.tar.gz";
-            if not $dl-exit-code == 0 {
-                die;
-            }
-        }
-        if "mecab-0.996".IO.d {
-            run 'rm', '-rf', 'mecab-0.996';
-        }
-        run 'tar', 'xvzf', 'mecab-0.996.tar.gz';
-        
+
+        my @fetch-backends = [
+            { module => "Zef::Service::Shell::wget" },
+            { module => "Zef::Service::Shell::curl" },
+        ];
+        my $fetcher      = Zef::Fetch.new(:backends(@fetch-backends));
+        my $uri          = 'https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7cENtOXlicTFaRUE';
+        my $archive-file = "mecab-0.996.tar.gz".IO.e
+        ?? "mecab-0.996.tar.gz"
+        !! $fetcher.fetch($uri, "mecab-0.996.tar.gz");
+
+        my @extract-backends = [
+            { module => "Zef::Service::Shell::tar" },
+            { module => "Zef::Service::Shell::p5tar" },
+        ];
+        my $extractor   = Zef::Extract.new(:backends(@extract-backends));
+        my $extract-dir = $extractor.extract($archive-file, $*CWD);
         if "mecab-0.996".IO.d {
             shell "patch -p1 mecab-0.996/src/mecab.h < $srcdir/mecab.h.patch";
             shell "patch -p1 mecab-0.996/src/libmecab.cpp < $srcdir/libmecab.cpp.patch";
@@ -52,32 +56,38 @@ class Build {
         run 'echo', "$prefix/lib", '>', '/etc/ld.so.conf.d/mecab.conf';
         chdir($goback);
     }
-    
+
     method !install-mecab-ipadic($workdir, $prefix) {
         my $goback = $*CWD;
         my $srcdir = "$workdir/src";
         my %vars = get-vars($workdir);
-        chdir($srcdir);
-        my $wget-exit-code = run 'which', 'wget';
-        if $wget-exit-code == 0 {
-            my $CMD = qq:x/which wget/.subst(/\s*/,"",:g);
-            my $url = "https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM";
-            my $dl-exit-code = run $CMD, $url, "-O", "mecab-ipadic-2.7.0-20070801.tar.gz";
-            if not $dl-exit-code == 0 {
-                die;
-            }
-        }
-        if "mecab-ipadic-2.7.0-20070801".IO.d {
-            run 'rm', '-rf', 'mecab-ipadic-2.7.0-20070801';
-        }
-        run 'tar', 'xvzf', 'mecab-ipadic-2.7.0-20070801.tar.gz';
+
+        my @fetch-backends = [
+            { module => "Zef::Service::Shell::wget" },
+            { module => "Zef::Service::Shell::curl" },
+        ];
+        my $fetcher      = Zef::Fetch.new(:backends(@fetch-backends));
+        my $uri          = 'https://drive.google.com/uc?export=download&id=0B4y35FiV1wh7MWVlSDBCSXZMTXM';
+        my $archive-file = "mecab-ipadic-2.7.0-20070801.tar.gz".IO.e
+        ?? "mecab-ipadic-2.7.0-20070801.tar.gz"
+        !! $fetcher.fetch($uri, "mecab-ipadic-2.7.0-20070801.tar.gz");
+
+        my @extract-backends = [
+            { module => "Zef::Service::Shell::tar" },
+            { module => "Zef::Service::Shell::p5tar" },
+        ];
+        my $extractor   = Zef::Extract.new(:backends(@extract-backends));
+        my $extract-dir = $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO.e
+        ?? $archive-file.IO.basename.subst(/\.tar\.gz/, '').IO
+        !! $extractor.extract($archive-file, $*CWD);
+
         chdir("mecab-ipadic-2.7.0-20070801");
         shell("./configure --with-charset=utf8 --prefix=$prefix --with-mecab-config=$prefix/bin/mecab-config");
         shell("make");
         shell("make install");
         chdir($goback);
     }
-    
+
     method isa($what) {
         return True if $what.^name eq 'Panda::Builder';
         callsame;
