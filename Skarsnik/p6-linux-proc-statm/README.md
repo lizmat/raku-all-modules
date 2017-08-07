620b2d34ad4b319387f9b0985d5f74a5eb9c0bd6
Moritz Lenz <moritz@faui2k3.org>
2017-08-07 05:10:01 +0000

    git subrepo clone --force https://github.com/Skarsnik/p6-linux-proc-statm.git Skarsnik/p6-linux-proc-statm
    subrepo:
      subdir:   "Skarsnik/p6-linux-proc-statm"
      merged:   "bbe27b2"
    upstream:
      origin:   "https://github.com/Skarsnik/p6-linux-proc-statm.git"
      branch:   "master"
      commit:   "bbe27b2"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- Skarsnik/p6-linux-proc-statm/README.md
+++ Skarsnik/p6-linux-proc-statm/README.md
@@ -1,17 +1,22 @@
-#Name Linux::Proc::Statm
+# Name Linux::Proc::Statm
 
-#Usage
+# Usage
 
 ```perl
- use Linux::Proc:Statm;
+ use Linux::Proc::Statm;
  my %meminfo = get-statm(42); #get info for the pid 42
  say %meminfo<data>; # data + stack size
  say get-statm.perl; #use $*PID
+ say get-statm-human<data>; # 54.552 kB
 ```
 
-Values are given in Kb.
+Values are given in Kb by default. You can change this behavior by passing a `:unit` named parameter with etheir b, k or m according to the unit you want.
 
-#Author
+```perl
+say get-statm-human(:unit<m>)<data>; # 53 mB
+```
+
+# Author
 
 Sylvain "Skarsnik" Colinet <scolinet@gmail.com>
 
--- Skarsnik/p6-linux-proc-statm/lib/Linux/Proc/Statm.pm
+++ Skarsnik/p6-linux-proc-statm/lib/Linux/Proc/Statm.pm
@@ -3,22 +3,37 @@ use NativeCall;
 module Linux::Proc::Statm {
   sub getpagesize is native(Str) returns int32 { * };
   my $pagesize = getpagesize();
+  my %convert = (b => 1, k => 1024, m => 1024 * 1024);
 
-  sub f($v) {
-    return Int($v * $pagesize / 1024);
+  sub make-human ($var, $unit) {
+    return $var.flip.comb(/.**1..3/).join('.').flip ~ ($unit eq 'b' ?? ""  !! " {$unit}B");
   }
-  sub get-statm($pid = $*PID) is export {
+  sub f($v, $unit) {
+    return Int($v * $pagesize / %convert{$unit});
+  }
+  sub get-statm($pid = $*PID, :$unit = 'k') is export {
+    die unless defined %convert{$unit};
     my %data;
     my $fh = open '/proc/' ~ $pid ~ '/statm';
     my $statm-str = $fh.lines[0];
     my @value = $statm-str.split(/\s/);
-    %data<size> = f(@value[0]);
-    %data<resident> = f(@value[1]);
-    %data<share> = f(@value[2]);
-    %data<text> = f(@value[3]);
-    %data<lib> = f(@value[4]);
-    %data<data> = f(@value[5]);
-    %data<dirty> = f(@value[6]);
+    %data<size> = f(@value[0], $unit);
+    %data<resident> = f(@value[1], $unit);
+    %data<share> = f(@value[2], $unit);
+    %data<text> = f(@value[3], $unit);
+    %data<lib> = f(@value[4], $unit);
+    %data<data> = f(@value[5], $unit);
+    %data<dirty> = f(@value[6], $unit);
     return %data;
+  }
+
+  sub get-statm-human($pid = $*PID, :$unit = 'k') is export {
+    die unless defined %convert{$unit};
+    my %data = get-statm(:unit($unit));
+    my %toret;
+    for %data.kv -> $k, $v {
+      %toret{$k} = make-human($v, $unit);
+    }
+    return %toret;
   }
 }
