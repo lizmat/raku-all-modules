6b650c0a75350eafa4abcda7c35fd559772834ca
Moritz Lenz <moritz@faui2k3.org>
2017-08-15 05:10:50 +0000

    git subrepo clone --force https://github.com/melezhik/sparrowdo.git melezhik/sparrowdo
    subrepo:
      subdir:   "melezhik/sparrowdo"
      merged:   "e954011852"
    upstream:
      origin:   "https://github.com/melezhik/sparrowdo.git"
      branch:   "master"
      commit:   "e954011852"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- melezhik/sparrowdo/examples/modules/case/story.bash
+++ melezhik/sparrowdo/examples/modules/case/story.bash
@@ -2,8 +2,5 @@ case=$(story_var case)
 opts=$(config opts)
 echo run case $case ...
 
-perl6 -I$project_root_dir/../lib $project_root_dir/../bin/sparrowdo \
---host=127.0.0.1 \
---sparrowfile=$project_root_dir/$case/sparrowfile \
-$opts
+sparrowdo --sparrowfile=$project_root_dir/$case/sparrowfile --no_sudo --local_mode
 
