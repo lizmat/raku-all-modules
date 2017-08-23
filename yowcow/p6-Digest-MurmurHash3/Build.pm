61e7c2b0519536144e336fce14276a6b6951a3b0
Moritz Lenz <moritz@faui2k3.org>
2017-08-23 05:09:46 +0000

    git subrepo clone (merge) --force git://github.com/yowcow/p6-Digest-MurmurHash3.git yowcow/p6-Digest-MurmurHash3
    subrepo:
      subdir:   "yowcow/p6-Digest-MurmurHash3"
      merged:   "ad2d0a67c1"
    upstream:
      origin:   "git://github.com/yowcow/p6-Digest-MurmurHash3.git"
      branch:   "master"
      commit:   "ad2d0a67c1"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- yowcow/p6-Digest-MurmurHash3/Build.pm
+++ yowcow/p6-Digest-MurmurHash3/Build.pm
@@ -1,9 +1,7 @@
-use Panda::Common;
-use Panda::Builder;
 use LibraryMake;
 use Shell::Command;
 
-class Build is Panda::Builder {
+class Build {
     method build($workdir) {
         my $makefiledir = "$workdir/src";
         my $destdir = "$workdir/resources";
