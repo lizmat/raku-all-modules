c5191267eee15bd8c9ad89c81122029f235ed396
Moritz Lenz <moritz@faui2k3.org>
2017-09-06 05:04:49 +0000

    git subrepo clone --force git://github.com/grondilu/clifford.git grondilu/clifford
    subrepo:
      subdir:   "grondilu/clifford"
      merged:   "add7a58411"
    upstream:
      origin:   "git://github.com/grondilu/clifford.git"
      branch:   "master"
      commit:   "add7a58411"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

---
+++ grondilu/clifford/js/test.js
@@ -0,0 +1,21 @@
+const examples = [
+    '1',
+    '3.14',
+    '-1',
+    'a',
+    'foo',
+    'a + b',
+    'x+y',
+    'a*(b+c)',
+    '2/3',
+    '2/7 + x**2/2',
+    '(a - b)(a + b)'
+];
+let Parser = require('./parser');
+
+let parser = new Parser();
+
+for (let example of examples) {
+    console.log(parser.parse(example).toString());
+}
+
