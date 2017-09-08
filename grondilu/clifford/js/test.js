e32755d6d683b41ce5175636f2f60e8cb13d1773
Moritz Lenz <moritz@faui2k3.org>
2017-09-08 05:04:51 +0000

    git subrepo clone --force git://github.com/grondilu/clifford.git grondilu/clifford
    subrepo:
      subdir:   "grondilu/clifford"
      merged:   "d75c67a8c4"
    upstream:
      origin:   "git://github.com/grondilu/clifford.git"
      branch:   "master"
      commit:   "d75c67a8c4"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- grondilu/clifford/js/test.js
+++ grondilu/clifford/js/test.js
@@ -1,20 +1,54 @@
 const examples = [
-    '1',
-    '3.14',
-    '-1',
+    '355/113',
+    'pi=355/113',
+    '1/2 + 3',
+    '1+2',
+    '2*(3+4)',
+    '3.14e0',
+    '3.14*e0',
     'a',
-    'foo',
-    'a + b',
-    'x+y',
-    'a*(b+c)',
-    '2/3',
-    '2/7 + x**2/2',
-    '(a - b)(a + b)'
+    'a+b',
+    'a*b',
+    'a b',
+    'x*e1',
+    '(a+b)**2',
+    '(a+b)∧c',
+    '(a*b)∧c',
+    'a*b∧c',
+    'x²',
+    'a**b**c',
+    'foo*(a-b)',
+    '3.14*r**2',
+    'u·v',
+    'no·ni',
+    'pi = 355/113',
+    '(a-b)(a+b)',
+    '1+u∧v',
+    'no∧ni',
+    'e0**2',
+    'e0*e1',
+    'e1*e1',
+    'e0·e1',
+    'e0·e0',
+    'ē0',
+    'ē0·ē0',
+    'ē0·ē1',
+    'ē0·e1',
+    'e1·ē1',
+    'a·b∧c',
+    'no + x*e1 + y*e2 + z*e3 + (x² + y² + z²)/2*ni'
 ];
-let Parser = require('./parser'),
-    parser = new Parser();
 
+
+let parser = require('./clifford').parser,
+    errors = 0;
 for (let example of examples) {
-    console.log(parser.parse(example).toString());
+    try {
+        let ast = parser.parse(example);
+        console.log(`"${example}" parsed as ${ast.simplify()}`);
+    } catch (e) {
+        errors++;
+        console.log(`"${example}": ${e}`);
+    }
 }
-
+console.log(`${errors} errors out of ${examples.length} attempts`);
