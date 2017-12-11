e8af19263403766c593f8983b9381c93330fe2c9
Moritz Lenz <moritz@faui2k3.org>
2017-12-11 05:00:36 +0000

    add or update cpan/WARRINGD/PDF-Font

--- cpan/WARRINGD/PDF-Font/lib/PDF/Font.pm
+++ cpan/WARRINGD/PDF-Font/lib/PDF/Font.pm
@@ -59,84 +59,9 @@ class PDF::Font {
 
 PDF::Font
 
-=head1 SYNPOSIS
-
- use PDF::Lite;
- use PDF::Font;
- my $deja = PDF::Font.load-font: :file<t/fonts/DejaVuSans.ttf>;
-
- # requires fontconfig
- my $deja-vu = PDF::Font.load-font: :name<DejaVuSans>;
-
- my PDF::Lite $pdf .= new;
- $pdf.add-page.text: {
-    .font = $deja;
-    .text-position = [10, 600];
-    .say: 'Hello, world';
- }
- $pdf.save-as: "/tmp/example.pdf";
-
 =head1 DESCRIPTION
 
-This module provdes font handling for
-L<PDF::Lite>,  L<PDF::API6> and other PDF modules.
-
-=head1 METHODS
-
-
-=head3 load-font
-
-A class level method to create a new font object.
-
-=head4 C<PDF::Font.load-font(Str :$file);>
-
-Loads a font file.
-
-parameters:
-=begin item
-C<:$file>
-
-Font file to load. Currently supported formats are:
-=item2 Open-Type (C<.otf>)
-=item2 True-Type (C<.ttf>)
-=item2 Postscript (C<.pfb>, or C<.pfa>)
-
-=end item
-
-=head4 C<PDF::Font.load-font(Str :$name);>
-
- my $vera = PDF::Font.load-font('vera');
- my $deja = PDF::Font.load-font('Deja:weight=bold:width=condensed:slant=italic');
-
-Loads a font by a fontconfig name.
-
-Note: Requires fontconfig to be installed on the system.
-
-parameters:
-=begin item
-C<:$name>
-
-Name of an installed system font to load.
-
-=end item
-
-=head3 find-font
-
-  find-font(Str $family-name,
-            Str :$weight,     # thin|extralight|light|book|regular|medium|semibold|bold|extrabold|black
-            Str :$stretch,    # normal|[ultra|extra]?[condensed|expanded]
-            Str :$slant,      # normal|oblique|italic
-            );
-
-Locates a matching font-file. Doesn't actually load it.
-
-   my $file = PDF::Font.find-font('Deja', :weight<bold>, :width<condensed>, :slant<italic>);
-   say $file;  # /usr/share/fonts/truetype/dejavu/DejaVuSansCondensed-BoldOblique.ttf
-   my $font = PDF::Font.load-font( :$file )';
-
-=head1 BUGS AND LIMITATIONS
-
-=item Font subsetting is not yet implemented. I.E. fonts are always fully embedded, which may result in large PDF files.
+B<This module has been renamed. Please use PDF::Font::Loader>
 
 =end pod
 
