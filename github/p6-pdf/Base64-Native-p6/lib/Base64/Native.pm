f42733228021c95943a0d5a94eed8f9a343b7139
Moritz Lenz <moritz@faui2k3.org>
2018-03-04 05:20:08 +0000

    git subrepo clone --force git://github.com/p6-pdf/Base64-Native-p6.git github/p6-pdf/Base64-Native-p6
    subrepo:
      subdir:   "github/p6-pdf/Base64-Native-p6"
      merged:   "84c0634246"
    upstream:
      origin:   "git://github.com/p6-pdf/Base64-Native-p6.git"
      branch:   "master"
      commit:   "84c0634246"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- github/p6-pdf/Base64-Native-p6/lib/Base64/Native.pm
+++ github/p6-pdf/Base64-Native-p6/lib/Base64/Native.pm
@@ -29,17 +29,17 @@ sub dec-alloc(Blob $in) {
 
 our proto sub base64-encode($, $?, :$enc, :$str, :$uri)  is export { * }
 
+multi sub base64-encode(Str $in, :$enc = 'utf8', |c) {
+    base64-encode($in.encode($enc), |c)
+}
 multi sub base64-encode(:$str! where .so, |c --> Str) {
     base64-encode(|c).decode;
-} 
-multi sub base64-encode(Blob $in, Blob $out = enc-alloc($in), :$uri --> Blob) {
+}
+multi sub base64-encode(Blob $in, Blob $out = enc-alloc($in), :$uri --> Blob) is default {
     $uri
 	?? base64_encode_uri($in, $in.bytes, $out, $out.bytes)
 	!! base64_encode($in, $in.bytes, $out, $out.bytes);
     $out;
-}
-multi sub base64-encode(Str $in, :$enc = 'utf8', |c --> Blob) {
-    base64-encode($in.encode($enc), |c)
 }
 
 our proto sub base64-decode($, $?, :$enc)  is export { * }
@@ -50,7 +50,7 @@ multi sub base64-decode(Str :$enc!, |c --> Str) {
 multi sub base64-decode(Str $in, :$enc = 'utf8', |c --> Blob) {
     base64-decode($in.encode($enc), |c)
 }
-multi sub base64-decode(Blob $in, Blob $out = dec-alloc($in) --> Blob) {
+multi sub base64-decode(Blob $in, Blob $out = dec-alloc($in) --> Blob) is default {
     my ssize_t $n = base64_decode($in, $in.bytes, $out, $out.bytes);
     die "unable to decode as base64. stopped at byte {-$n}: 0x{$in[-$n - 1].base(16)} {$in[-$n - 1].chr.perl}"
 	if $n < 0;
