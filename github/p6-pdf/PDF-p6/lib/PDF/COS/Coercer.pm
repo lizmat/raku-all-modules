b34d8bc52a06c6216c9fd6cc77ed71015150f469
Moritz Lenz <moritz@faui2k3.org>
2018-04-07 05:13:55 +0000

    git subrepo clone --force git://github.com/p6-pdf/PDF-p6.git github/p6-pdf/PDF-p6
    subrepo:
      subdir:   "github/p6-pdf/PDF-p6"
      merged:   "574e28e4e1"
    upstream:
      origin:   "git://github.com/p6-pdf/PDF-p6.git"
      branch:   "master"
      commit:   "574e28e4e1"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

--- github/p6-pdf/PDF-p6/lib/PDF/COS/Coercer.pm
+++ github/p6-pdf/PDF-p6/lib/PDF/COS/Coercer.pm
@@ -53,19 +53,12 @@ class PDF::COS::Coercer {
 	self.coerce( $obj, r);
     }
 
-    multi method coerce( Array $obj is copy, PDF::COS::Tie::Array  $role) {
-        $obj = PDF::COS.coerce($obj) unless $obj ~~ PDF::COS;
-	$obj.mixin: $role;
+    multi method coerce( Array $obj is copy, PDF::COS::Tie::Array  $role where !.isa(PDF::COS::Array)) {
+        PDF::COS.coerce($obj).mixin: $role;
     }
 
-    multi method coerce( Hash $obj is copy, PDF::COS::Tie::Hash $role) {
-        $obj = PDF::COS.coerce($obj) unless $obj ~~ PDF::COS;
-	$obj.mixin: $role;
-    }
-
-    multi method coerce( $obj, $type where PDF::COS::Tie ) {
-	warn X::PDF::Coerce.new( :$obj, :$type );
-        $obj;
+    multi method coerce( Hash $obj is copy, PDF::COS::Tie::Hash $role where !.isa(PDF::COS::Dict)) {
+        PDF::COS.coerce($obj).mixin: $role;
     }
 
     multi method coerce( $obj, $type) {
