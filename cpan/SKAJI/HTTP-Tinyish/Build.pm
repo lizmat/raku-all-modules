1ff52ca828bb467d9c0f4b68973713da3bd4caeb
Moritz Lenz <moritz@faui2k3.org>
2018-04-30 05:00:25 +0000

    add or update cpan/SKAJI/HTTP-Tinyish

--- cpan/SKAJI/HTTP-Tinyish/Build.pm
+++ cpan/SKAJI/HTTP-Tinyish/Build.pm
@@ -3,7 +3,7 @@ unit class Build;
 
 method build($dist-path) {
     my $proc = run "curl", "--version", :out, :err;
-    return if $proc.exitcode == 0;
+    return True if $proc.exitcode == 0;
 
     die "This module requires 'curl' command, but couldn't find it, abort.";
 }
--- cpan/SKAJI/HTTP-Tinyish/lib/HTTP/Tinyish.pm6
+++ cpan/SKAJI/HTTP-Tinyish/lib/HTTP/Tinyish.pm6
@@ -1,5 +1,5 @@
 use v6;
-unit class HTTP::Tinyish:ver<0.1.0>;
+unit class HTTP::Tinyish:ver<0.1.2>:auth<cpan:SKAJI>;
 use HTTP::Tinyish::Curl;
 
 has $.backend handles <request get head put post delete mirror>;
