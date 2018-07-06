ccd3c9315eceaf8648ee80ae5801938f65e54e4c
Moritz Lenz <moritz@faui2k3.org>
2018-07-06 08:03:10 +0000

    git subrepo clone --force git://github.com/nkh/P6-Grammar-Tracer-Compact.git github/nkh/P6-Grammar-Tracer-Compact
    subrepo:
      subdir:   "github/nkh/P6-Grammar-Tracer-Compact"
      merged:   "9f96614492"
    upstream:
      origin:   "git://github.com/nkh/P6-Grammar-Tracer-Compact.git"
      branch:   "master"
      commit:   "9f96614492"
    git-subrepo:
      version:  "0.4.0"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "b645cc9"

---
+++ github/nkh/P6-Grammar-Tracer-Compact/lib/Grammar/Tracer/Compact.pm
@@ -0,0 +1,55 @@
+use Terminal::ANSIColor;
+
+# On Windows you can use perl 5 to get proper output:
+# - send through Win32::Console::ANSI: perl6 MyGrammar.pm | perl -e "use Win32::Console::ANSI; print while (<>)"
+# - to strip all the escape codes:     perl6 MyGrammar.pm | perl -e "print s/\e\[[0-9;]+m//gr while (<>)"
+
+my class TracedGrammarHOW is Metamodel::GrammarHOW {
+    my $level = 0;
+    my @traces;
+
+    method find_method($obj, $name) {
+        my $meth := callsame;
+        return $meth if $meth.WHAT.^name eq 'NQPRoutine';
+        return $meth unless $meth ~~ Any;
+        return $meth unless $meth ~~ Regex;
+        return -> $c, |args {
+
+            @traces = () if $level == 0 ; # reset between grammar matches
+
+            @traces.push: 'this' ;
+            my $this := @traces[*-1] ; # @traces has elements added by sub matches, remember where to update
+            
+	    my $indentation = '   ' x $level ;
+
+            # Call rule.
+            $level++;
+            my $result := $meth($obj, |args);
+            $level--;
+
+            if $result.MATCH {
+                #my $width = 79 - (3 * $level);
+
+                $this = $indentation ~ colored($name, 'bold green') ~ RESET( ) ~ ' '
+                                   #~ ( $result.MATCH.Str.substr(0, $width).perl // '' )
+                                   ~ ( $result.MATCH.Str.perl // '' )
+            }
+            else{
+                $this = $indentation ~ colored($name, 'bold red') ~ RESET()
+            }
+
+            # Dump result.
+            @traces.join("\n").say if $level == 0 ;
+
+            $result
+        }
+    }
+    
+    method publish_method_cache($obj) {
+        # Suppress this, so we always hit find_method.
+    }
+}
+
+# Export this as the meta-class for the "grammar" package declarator.
+my module EXPORTHOW { }
+EXPORTHOW::<grammar> = TracedGrammarHOW;
