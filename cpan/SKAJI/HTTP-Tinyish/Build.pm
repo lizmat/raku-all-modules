030750d55b1bb4b52ab2053830689f15f5a27652
Moritz Lenz <moritz@faui2k3.org>
2018-07-07 20:04:12 +0000

    add or update cpan/SKAJI/HTTP-Tinyish

--- cpan/SKAJI/HTTP-Tinyish/Build.pm
+++
@@ -1,9 +0,0 @@
-use v6.c;
-unit class Build;
-
-method build($dist-path) {
-    my $proc = run "curl", "--version", :out, :err;
-    return True if $proc.exitcode == 0;
-
-    die "This module requires 'curl' command, but couldn't find it, abort.";
-}
--- cpan/SKAJI/HTTP-Tinyish/t/01-basic.t
+++
@@ -1,7 +0,0 @@
-use v6;
-use Test;
-use HTTP::Tinyish;
-
-pass "ok";
-
-done-testing;
