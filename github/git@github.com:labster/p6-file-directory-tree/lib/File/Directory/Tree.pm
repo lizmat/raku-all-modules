eb1c9884fb0eb9a5c19c62cb26f6526cc7b1d2d2
Moritz Lenz <moritz.lenz@gmail.com>
2018-10-27 19:02:41 +0000

    git subrepo clone --force git@github.com:labster/p6-file-directory-tree.git github/git@github.com:labster/p6-file-directory-tree
    subrepo:
      subdir:   "github/git@github.com:labster/p6-file-directory-tree"
      merged:   "25faef927c"
    upstream:
      origin:   "git@github.com:labster/p6-file-directory-tree.git"
      branch:   "master"
      commit:   "25faef927c"
    git-subrepo:
      version:  "0.3.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "a7ee886"

---
+++ github/git@github.com:labster/p6-file-directory-tree/lib/File/Directory/Tree.pm
@@ -0,0 +1,41 @@
+unit module File::Directory::Tree;
+
+multi sub mktree (Cool:D $path is copy, Int :$mask = 0o777 ) is export {
+	return True if $path.IO ~~ :d;
+	$path.=IO;
+	my @makedirs;
+	while $path !~~ :e {
+		@makedirs.push($path);
+		$path.=parent;
+	}
+	for @makedirs.reverse -> $dir {
+		mkdir($dir, $mask) or return False unless $dir.e;
+	}
+	True;
+}
+
+multi sub empty-directory (Cool:D $path is copy) {
+    empty-directory $path.IO;
+}
+
+multi sub empty-directory (IO::Path:D $path) is export {
+	$path.d or fail "$path is not a directory";
+	for eager $path.dir -> $file {
+		#say $file.perl;
+		if $file.l.not and $file.d { rmtree $file }
+		else { unlink $file }
+	}
+	True;
+}
+
+multi sub rmtree (Cool:D $path is copy) {
+    rmtree $path.IO;
+}
+
+multi sub rmtree (IO::Path:D $path) is export {
+	return True if !$path.e;
+	$path.d or fail "$path is not a directory";
+	empty-directory($path.IO) or return False;
+	rmdir($path.IO) or return False;
+	True;
+}
