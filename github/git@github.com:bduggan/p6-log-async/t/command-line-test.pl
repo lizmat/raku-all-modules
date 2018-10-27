47e9c4d9755fc84404d59412c451e9c82cca9153
Moritz Lenz <moritz.lenz@gmail.com>
2018-10-27 19:36:26 +0000

    git subrepo clone --force git@github.com:bduggan/p6-log-async.git github/git@github.com:bduggan/p6-log-async
    subrepo:
      subdir:   "github/git@github.com:bduggan/p6-log-async"
      merged:   "c049ed705b"
    upstream:
      origin:   "git@github.com:bduggan/p6-log-async.git"
      branch:   "master"
      commit:   "c049ed705b"
    git-subrepo:
      version:  "0.3.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "a7ee886"

---
+++ github/git@github.com:bduggan/p6-log-async/t/command-line-test.pl
@@ -0,0 +1,20 @@
+use v6;
+
+use Log::Async;
+use Log::Async::CommandLine;
+use TestModule;
+
+trace   'log trace';
+debug   'log debug';
+info    'log info';
+warning 'log warning';
+error   'log error';
+fatal   'log fatal';
+
+say "ARGS: {@*ARGS.join(',')}";
+
+if '--testsubs' ∈ @*ARGS
+{
+    sub_a;
+    sub_b;
+}
