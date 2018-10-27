7605083d0adf9e6baba975e5037c47954ee2fabd
Moritz Lenz <moritz.lenz@gmail.com>
2018-10-27 19:26:24 +0000

    git subrepo clone --force git@github.com:jonathanstowe/Audio-Liquidsoap.git github/git@github.com:jonathanstowe/Audio-Liquidsoap
    subrepo:
      subdir:   "github/git@github.com:jonathanstowe/Audio-Liquidsoap"
      merged:   "4c2f108deb"
    upstream:
      origin:   "git@github.com:jonathanstowe/Audio-Liquidsoap.git"
      branch:   "master"
      commit:   "4c2f108deb"
    git-subrepo:
      version:  "0.3.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "a7ee886"

---
+++ github/git@github.com:jonathanstowe/Audio-Liquidsoap/t/lib/RunServer.pm
@@ -0,0 +1,61 @@
+use v6;
+
+class RunServer {
+
+    use File::Which;
+
+    has Int     $.port = 1234;
+    has Str     $.script;
+    has Str     $.liquidsoap;
+    has Supply  $.stdout;
+    has Supply  $.stderr;
+
+    has Proc::Async $!proc;
+    has Promise     $.Promise;
+
+    class X::NoSoap is Exception {
+        has $.message = "Can't find a 'liquidsoap' to run";
+    }
+    
+    multi submethod BUILD(Int :$!port = 1234, Str :$!script, Str :$liquidsoap) {
+        $!liquidsoap = %*ENV<LIQUIDSOAP> // $liquidsoap // which('liquidsoap');
+
+        if not ( $!liquidsoap.defined && $!liquidsoap.IO.x ) {
+            X::NoSoap.new.throw;
+        }
+
+        my $set-port = "set('server.telnet.port',{ $!port })";
+
+        my @args = '--enable-telnet', '--force-start', '--verbose', $set-port, $!script // Empty;
+
+        $!proc = Proc::Async.new($!liquidsoap, @args);
+        $!stdout = $!proc.stdout;
+        $!stderr = $!proc.stderr;
+    }
+
+    
+    multi method run(RunServer:D:) {
+        $!Promise = $!proc.start;
+        my $s = self;
+        my $t = signal(SIGINT, SIGHUP).tap({ $t.close; sleep 1; $s.kill });
+        True;
+    }
+
+    multi method run(RunServer:U: |c) returns RunServer {
+        my $runner = self.new(|c);
+        $runner.run;
+        $runner;
+    }
+
+    method kill() {
+        if $!proc.defined {
+            $!proc.kill('INT');
+            True;
+        }
+        else {
+            False;
+        }
+    }
+}
+
+# vim: expandtab shiftwidth=4 ft=perl6
